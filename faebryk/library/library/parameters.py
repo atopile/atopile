58810da4cb24581f421c39784ccf61e1a4ea8ae5
RubenB-ITENG <94007802+ruben-iteng@users.noreply.github.com>
2022-06-03 11:28:00 +0200

    Library: Add Range parameter (#91)
    Add a ranged parameter (min/max)

--- faebryk/library/library/parameters.py
+++ faebryk/library/library/parameters.py
@@ -8,6 +8,7 @@ logger = logging.getLogger("library")
 from faebryk.library.traits.parameter import is_representable_by_single_value
 from faebryk.library.core import Parameter
 from faebryk.library.traits import *
+from faebryk.libs.exceptions import FaebrykException
 import typing
 
 
@@ -16,6 +17,21 @@ class Constant(Parameter):
         super().__init__()
         self.value = value
         self.add_trait(is_representable_by_single_value(self.value))
+
+
+class Range(Parameter):
+    def __init__(self, value_min: typing.Any, value_max: typing.Any) -> None:
+        super().__init__()
+        self.min = value_min
+        self.max = value_max
+
+    def pick(self, value_to_check: typing.Any):
+        if not self.min <= value_to_check <= self.max:
+            raise FaebrykException(
+                f"Value not in range: {value_to_check} not in [{self.min},{self.max}]"
+            )
+
+        self.add_trait(is_representable_by_single_value(value_to_check))
 
 
 class TBD(Parameter):
