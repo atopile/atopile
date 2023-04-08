dde325c5b828deb4776040cbf28ef8661a85999e
Matt Wildoer <mattwildoer@Matts-MacBook-Air.local>
2023-04-08 16:12:33 -0700

    add data model, restructure to seperate notes and examples

---
+++ notes/20230408/skidl_notes.md
@@ -0,0 +1,14 @@
+# skidl notes:
+
+## bad:
+- the way components are defined and imported sucks. There's a lack of links between the footprint, schematic, symbol, spice model etc...
+- the layout should be well linked to original source, so that changes propagate well without having to redo your entire layout
+- it's too easy to make mistakes in the schmematic and not be able to see them
+- there's no good way to visualise the circuits you're designing
+- connecting a pin at a time sucks, we want busses (I know skidl has them, but 🤷‍♂️)
+- it quickly becomes unclear what's connected to what
+- unconnected components shouldn't be part of a circuit by default
+## good:
+- it was intuative to use - pythony, almost english descriptive
+- word operators are nice
+- liked being able to configure modules
---
+++ src/atopile/datamodel.py
@@ -0,0 +1,69 @@
+from attrs import define
+from typing import List
+
+@define
+class Pin:
+    name: str
+
+@define
+class ConcretePin(Pin):
+    ref: str
+
+@define
+class TransferFunction:
+    eqn: str
+
+@define
+class Limit:
+    eqn: str
+
+@define
+class Type:
+    name: str
+    pin: Pin
+
+@define
+class State:
+    name: str
+    transfer_functions: List[TransferFunction]
+    limits: List[Limit]
+    type: Type
+
+@define
+class Argument:
+    name: str
+    unit: str
+
+@define
+class BaseConfigurable:
+    args: List[Argument]
+
+@define
+class Feature(BaseConfigurable):
+    name: str
+    pins: List[Pin]
+    transfer_functions: List[TransferFunction]
+    limits: List[Limit]
+    states: List[State]  # states are always concrete
+
+@define
+class ConcreteFeature(Feature):
+    pins: List[ConcretePin]
+    types: List[Type]
+
+@define
+class Component(BaseConfigurable):
+    name: str
+    pins: List[ConcretePin]
+    transfer_functions: List[TransferFunction]
+    types: List[Type]
+    limits: List[Limit]
+    states: List[State]
+
+    # all available features for this component
+    features: List[ConcreteFeature]
+
+@define
+class ConcreteComponent(Component):
+    component: Component
+    features: List[ConcreteFeature] # enabled features
