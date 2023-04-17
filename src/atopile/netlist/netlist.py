de6e5b912784586c6523735cfa0cb2829e937129
Timot05 <timothee.peter@mac.com>
2023-04-17 09:27:11 -0700

    done adding class object

--- src/atopile/netlist/netlist.py
+++ src/atopile/netlist/netlist.py
@@ -80,36 +80,16 @@ class kicad_netlist:
         self.tool = tool
 
     def add_component_to_netlist(self, component: kicad_component) -> None:
+        
         self.components.append(component)
 
     def add_component_prototype_to_netlist(self, component_prototype: kicad_component_prototype) -> None:
-        # pin_list = []
-        # for pin in pins:
-        #     pin_dict = {}
-        #     pin_dict['num'] = pin.num
-        #     pin_dict['name'] = pin.name
-        #     pin_dict['pin_type'] = pin.type
-
-        #     pin_list.append(pin_dict)
-
-        # fields_dict = {}
-        # for field in fields:
-        #     fields_dict[field.name] = field.value
 
         self.component_prototypes.append(component_prototype)
 
-    def add_net_to_netlist(self, code: int, name: str, nodes: list) -> None:
-        node_list = []
-        for node in nodes:
-            node_dict = {}
-            node_dict['ref'] = node.ref
-            node_dict['pin'] = node.pin
-            node_dict['pinfunction'] = node.pin_function
-            node_dict['pintype'] = node.pin_type
+    def add_net_to_netlist(self, net: kicad_net) -> None:
 
-            node_list.append(node_dict)
-
-        self.nets.append({'code' : code, 'name' : name, 'nodes' : node_list})
+        self.nets.append(net)
 
     def generate_completed_netlist(self) -> None:
         # Create a Jinja2 environment
@@ -153,21 +133,10 @@ test_pin1 = kicad_pin(num = 1, name='pin1', pin_type = 'active')
 
 test_component = kicad_component(name='test1', value=1, fields=[test_field1, test_field2])
 test_prototype_component = kicad_component_prototype(lib="lib", part='this is a part', docs='this is docs', footprint=['fp1', 'fp2'], fields=[test_field1, test_field2], pins = [test_pin1])
+test_net = kicad_net(code = 1, name = '+1v1',nodes = [test_node1, test_node2])
 
 a_netlist.add_component_to_netlist(test_component)
 a_netlist.add_component_prototype_to_netlist(test_prototype_component)
-a_netlist.add_net_to_netlist(code = 1, name = '+1v1',nodes = [test_node1, test_node2])
+a_netlist.add_net_to_netlist(test_net)
+
 a_netlist.generate_completed_netlist()
-
-# comps = [ {'name' : 'test1', 'value' : 1, 'fields' : { "LCSC" : "C002" , 'a field' : 'test'}}, {'name' : 'test2' , 'value' : 2, 'fields' : { "LCSC" : "C001" }} ]
-# components_string = comp_template.render(comps=comps)
-
-# # Load the libpart template and render
-# libpart_template = env.get_template('libpart_template.j2')
-# libparts = [ {'lib' : 'library', 'part' : 'this is a template' , 'docs' : 'this is a doc' , 'footprints' : ['footprint1', 'footprint2'] , 'fields' : { "LCSC" : "C002" , 'a field' : 'test'}, 'pins' : [{'num' : '1' , 'name' : 'p1' , 'type' : 'passive'}]} ]
-# libparts_string = libpart_template.render(libparts=libparts)
-
-# # Load the net template and render
-# net_template = env.get_template('net_template.j2')
-# nets = [ {'code' : '1' , 'name' : '+1V1', 'nodes' : [ {'ref' : 'C10' , 'pin' : 1, 'pinfunction' : 'does smth' , 'pintype' : 'passive'}, {'ref' : 'C8' , 'pin' : 1, 'pintype' : 'passive'}] } , {'code' : '2' , 'name' : '+2V', 'nodes' : [ {'ref' : 'C10' , 'pin' : 1, 'pintype' : 'passive'}, {'ref' : 'C8' , 'pin' : 1, 'pintype' : 'passive'}] }]
-# nets_string = net_template.render(nets=nets)
