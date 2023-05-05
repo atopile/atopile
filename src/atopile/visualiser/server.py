1b0d55a79e237abd2013eccb7ecbab53c0050980
Matt Wildoer <mattwildoer@Matts-MacBook-Air.local>
2023-05-05 09:08:09 -0700

    add skeleton visualiser

---
+++ src/atopile/visualiser/server.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="src/visualiser_client/static"), name="static")
+
+@app.get("/api/")
+async def api_root():
+    return {"message": "Hello World"}
+
+@app.get("/api/graph")
+async def graph():
+    return {
+        "cells": [{
+            "id": 1,
+            "type": 'standard.Rectangle',
+            "position": {
+                "x": 100,
+                "y": 100
+            },
+            "size": {
+                "width": 100,
+                "height": 100
+            }
+        }]
+    }
