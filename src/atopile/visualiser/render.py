4367957e4646c087d9b8d3e359f2bae2d8b68fd5
Timot05 <timothee.peter@mac.com>
2023-05-06 10:53:40 -0700

    rough debug graph generation

--- src/atopile/visualiser/render.py
+++ src/atopile/visualiser/render.py
@@ -1,6 +1,104 @@
 # for now, statically import the "toy_model" or something you want to use as a demo
 from atopile.data.toy_model import m as toy_model
+from atopile.model.utils import EDGE_COLOR_DICT, VERTEX_COLOR_DICT
+
+import json as json
+
+def igraph_to_render_coord_transform(coords):
+    new_coords = []
+    for coord in coords:
+        new_coords.append(70 * (coord + 7))
+
+    return new_coords
 
 ## in this file we should make a jointjs dict
 def render():
-    pass
+    layout = toy_model.graph.layout()
+
+    # Get the position of vertex 0
+    pos = layout[0]
+
+    print(pos)
+    print('this is a test')
+
+    verticies = toy_model.graph.vs
+    print(type(verticies))
+
+    rendered_graph_json = {}
+    rendered_graph_json['cells'] = []
+
+    for vertex in verticies:
+        vertex_position = igraph_to_render_coord_transform(layout[vertex.index])
+        vertex_color = VERTEX_COLOR_DICT[vertex['type']]
+        vertex_json_dict = {
+            "id": vertex.index+1,
+            "type": 'standard.Circle',
+            "position": {
+                "x": vertex_position[0],
+                "y": vertex_position[1]
+            },
+            "size": {
+                "width": 50,
+                "height": 50
+            },
+            "attrs": {
+                "body": {
+                "fill": vertex_color
+                },
+                "label": {
+                    "text": vertex['ref'],    
+                    'font-family': 'Helvetica',
+                    'font-size': 10
+                }
+            }
+        }
+        
+        rendered_graph_json['cells'].append(vertex_json_dict)
+
+        #print(rendered_graph_json['cells'])
+    
+    edges = toy_model.graph.es
+
+    for edge in edges:
+        edge_source = edge.source+1
+        edge_target = edge.target+1
+        edge_color = EDGE_COLOR_DICT[edge['type']]
+        edge_json_dict = {
+            "type": "standard.Link",
+            "source": {
+                "id": edge_source
+            },
+            "target": {
+                "id": edge_target
+            },
+            "labels": [{
+                "attrs": {
+                    'text': {
+                        'text': edge['type'],
+                        'font-family': 'Helvetica',
+                        'font-size': 10
+
+                    }}}],
+            "attrs": {
+                "line": {
+                    "stroke": edge_color,
+                    'stroke-width': 2
+                },
+            }
+        }
+        
+        rendered_graph_json['cells'].append(edge_json_dict)
+
+
+    # Open a file for writing
+    with open("src/visualiser_client/static/graph.json", "w") as f:
+    # Write the dictionary to the file as a JSON object
+        json.dump(rendered_graph_json, f)
+        
+
+
+# In here, we get the graph, for each vertex in the graph, we generate
+# an onject in the JSON. We then connect the edges together 
+
+if __name__ == "__main__":
+    render()
--- src/atopile/visualiser/server.py
+++ src/atopile/visualiser/server.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from atopile.visualiser.render import render
+
+import json as json
 
 app = FastAPI()
 
@@ -9,20 +12,47 @@ app.mount("/static", StaticFiles(directory="src/visualiser_client/static"), name
 async def api_root():
     return {"message": "Hello World"}
 
-@app.get("/api/graph")
-async def get_graph():
-    # call render.py to get a dict of the current circuit
-    return {
-        "cells": [{
-            "id": 1,
-            "type": 'standard.Rectangle',
-            "position": {
-                "x": 200,
-                "y": 100
-            },
-            "size": {
-                "width": 100,
-                "height": 100
-            }
-        }]
-    }
+# @app.get("/api/graph")
+# async def get_graph():
+#     # call render.py to get a dict of the current circuit
+#     with open("output.json", "r") as f:
+#     # Load the contents of the file into a Python object
+#         data = json.load(f)
+#     return data
+    # return {
+    #     "cells": [
+    # {
+    #   "type": "standard.Rectangle",
+    #   "position": {
+    #     "x": 100,
+    #     "y": 30
+    #   },
+    #   "size": {
+    #     "width": 100,
+    #     "height": 40
+    #   },
+    #   "angle": 0,
+    #   "id": "1f9f2843-2015-4784-a1d5-c3745423b5e1",
+    #   "z": 1,
+    #   "attrs": {
+    #     "body": {
+    #       "fill": "blue"
+    #     },
+    #     "label": {
+    #       "fill": "white",
+    #       "text": "Hello"
+    #     }}},
+        
+    #     {
+    #         "id": 1,
+    #         "type": 'standard.Rectangle',
+    #         "position": {
+    #             "x": 200,
+    #             "y": 100
+    #         },
+    #         "size": {
+    #             "width": 100,
+    #             "height": 100
+    #         }
+    #     }]
+    # }
