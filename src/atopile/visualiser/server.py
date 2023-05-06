483fd1178e2afe045029a7793a3b824f17b8cd57
Matt Wildoer <mattwildoer@Matts-MacBook-Air.local>
2023-05-06 15:17:02 -0700

    add feedback path

--- src/atopile/visualiser/server.py
+++ src/atopile/visualiser/server.py
@@ -1,8 +1,15 @@
+import json as json
+import logging
+from typing import Any, Dict
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+
 from atopile.visualiser.render import render
 
-import json as json
+# FIXME: https://github.com/tiangolo/fastapi/issues/1508
+log = logging.Logger(__name__, level=logging.DEBUG)
+log.warning("Test!")  # FIXME: this should be debug, but can't be because the logs don't output otherwise
 
 app = FastAPI()
 
@@ -12,6 +19,11 @@ app.mount("/static", StaticFiles(directory="src/visualiser_client/static"), name
 async def api_root():
     return {"message": "Hello World"}
 
+@app.post("/api/graph")
+async def post_graph(data: Dict[Any, Any]):
+    # this is where the `data` comes back to!
+    log.warning(str(data))  # FIXME:
+    return {"message": "OK"}
 # @app.get("/api/graph")
 # async def get_graph():
 #     # call render.py to get a dict of the current circuit
@@ -42,7 +54,6 @@ async def api_root():
     #       "fill": "white",
     #       "text": "Hello"
     #     }}},
-        
     #     {
     #         "id": 1,
     #         "type": 'standard.Rectangle',
