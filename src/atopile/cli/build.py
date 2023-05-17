d884456da1801f2e9d71da361c29791f926fcac3
Timot05 <timothee.peter@mac.com>
2023-05-16 20:00:49 -0700

    fix the build file

--- src/atopile/cli/build.py
+++ src/atopile/cli/build.py
@@ -4,13 +4,6 @@ from pathlib import Path
 
 from atopile.parser.parser import Builder
 from atopile.project.project import Project
-from atopile.utils import get_project_root
-
-project = Project.from_path(get_project_root() / "sandbox/example_project")
-model = Builder(project).build(Path(get_project_root() / "sandbox/example_project/toy.ato"))
-
-netlist = KicadNetlist.from_model(model, "toy.ato/vdiv1")
-netlist.to_file(project.root / "toy.net")
 
 @click.command()
 @click.argument("path", type=click.Path(exists=True))
