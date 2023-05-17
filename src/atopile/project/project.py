1c7b90b6efe736ede76f10a8ce6553ede3c43f31
Matt Wildoer <mattwildoer@Matts-MacBook-Air.local>
2023-05-16 18:58:34 -0700

    make std lib

--- src/atopile/project/project.py
+++ src/atopile/project/project.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from typing import Optional, Tuple
 
+from atopile.utils import get_project_root
+
 CONFIG_FILENAME = 'ato.yaml'
 ATO_DIR_NAME = 'ato'
 MODULE_DIR_NAME = 'ato.yaml'
@@ -38,16 +40,28 @@ class Project:
         project_dir = resolve_project_dir(path)
         return cls(project_dir)
 
+    def get_std_lib_path(self):
+        # TODO: this will only work for editable installs
+        return get_project_root() / "src/standard_library"
+
     def get_import_search_paths(self, cwp: Optional[Path] = None):
-        if cwp is not None:
+        if cwp is None:
+            search_paths = [self.module_dir]
+        else:
             if cwp.is_dir():
-                return [cwp, self.module_dir]
+                search_paths = [cwp, self.module_dir]
             else:
-                return [cwp.parent, self.module_dir]
-        return [self.module_dir]
+                search_paths = [cwp.parent, self.module_dir]
+        search_paths += [self.get_std_lib_path()]
+        return search_paths
 
     def standardise_import_path(self, path: Path) -> Path:
-        return path.resolve().absolute().relative_to(self.root)
+        if path.is_relative_to(self.root):
+            return path.resolve().absolute().relative_to(self.root)
+        elif path.is_relative_to(self.get_std_lib_path()):
+            return path.resolve().absolute().relative_to(self.get_std_lib_path())
+        else:
+            raise ImportError("Import is outside the project directory and isn't part of the std lib")
 
     def resolve_import(self, name: str, cwp: Optional[Path] = None) -> Tuple[Path, Path]:
         non_relative_paths = []
