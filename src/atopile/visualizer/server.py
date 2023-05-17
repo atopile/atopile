eba47233c435814a85822198c78824b7302b9531
Matt Wildoer <mattwildoer@Matts-MacBook-Air.local>
2023-05-16 21:58:56 -0700

    auto-rebuild on std-lib changes too

--- src/atopile/visualizer/server.py
+++ src/atopile/visualizer/server.py
@@ -51,6 +51,12 @@ class AtoWatcher:
             self.rebuild()
         return self._visualizer_dict
 
+    @property
+    def model(self) -> Model:
+        if self._model is None:
+            self.rebuild()
+        return self._model
+
     def rebuild(self):
         log.info("Rebuilding model")
         self._model = Builder(self.project).build(self.entrypoint_file)
@@ -58,13 +64,13 @@ class AtoWatcher:
 
     async def _watch_files(self):
         try:
-            async for changes in watchfiles.awatch(self.project.root):
+            async for changes in watchfiles.awatch(self.project.root, self.project.get_std_lib_path()):
                 log.info("Changes detected in project directory.")
                 # figure out what source files have been updated
                 changed_src_files = []
                 for _, file in changes:
                     std_path = self.project.standardise_import_path(Path(file))
-                    if std_path in self._model.src_files:
+                    if std_path in self.model.src_files:
                         changed_src_files.append(std_path)
 
                 if changed_src_files:
