69af8adc444fc93f52261420e98d0674d05f75e3
Matt Wildoer <mattwildoer@Matts-MacBook-Air.local>
2023-05-21 19:10:35 -0700

    convert specified output path to Path in ato build command

--- src/atopile/cli/build.py
+++ src/atopile/cli/build.py
@@ -34,6 +34,8 @@ def build(path, entrypoint, output, debug: bool, references: bool):
     if not output:
         project.ensure_build_dir()
         output = (project.build_dir / path.name).with_suffix(".net")
+    else:
+        output = Path(output)
 
     netlist.to_file(output)
     log.info(f"Wrote netlist to {output}")
