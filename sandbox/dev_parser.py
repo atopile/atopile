354ee8725838d64c67539d2abfea5c2872a68538
Matt Wildoer <mattwildoer@Matts-MacBook-Air.local>
2023-05-25 23:36:34 -0700

    clean up sandbox a little

--- sandbox/dev_parser.py
+++ sandbox/dev_parser.py
@@ -3,13 +3,13 @@
 %autoreload 2
 from pathlib import Path
 
-from atopile.parser.parser import Builder
+from atopile.parser.parser import build_model
 from atopile.project.project import Project
 from atopile.utils import get_project_root
 
 #%%
 project = Project.from_path(get_project_root() / "sandbox/example_project")
-model = Builder(project).build(Path(get_project_root() / "sandbox/example_project/toy.ato"))
+model = build_model(project, project.config.build.default_config)
 model.plot(debug=True)
 
 # %%
