269efdde6dcb2cd35331bc19a2fbfb55e7e6c309
Matt Wildoer <mattwildoer@Matts-MacBook-Air.local>
2023-06-25 21:17:13 -0700

    sandbox fix

--- sandbox/dev_parser.py
+++ sandbox/dev_parser.py
@@ -6,10 +6,20 @@ from pathlib import Path
 from atopile.parser.parser import build_model
 from atopile.project.project import Project
 from atopile.utils import get_project_root
+from atopile.project.config import CustomBuildConfig
 
 #%%
 project = Project.from_path(get_project_root() / "sandbox/example_project")
-model = build_model(project, project.config.builds.default)
+model = build_model(
+    project,
+    CustomBuildConfig(
+        "test",
+        project,
+        root_file=Path("example_project/toy.ato"),
+        root_node="toy.ato/Vdiv",
+        targets=["designators", "netlist-kicad6", "bom-jlcpcb"],
+    )
+)
 model.plot(debug=True)
 
 # %%
