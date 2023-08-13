eb6020556e681fd5024a1e5efd544b3254a20195
Timot05 <timothee.peter@mac.com>
2023-08-13 08:57:16 -0700

    remove path tools from render script

--- src/viewer/render.js
+++ src/viewer/render.js
@@ -3,6 +3,11 @@
 //import { settings_dict } from "./vis_settings";
 
 import { shapes, util, dia, anchors } from 'jointjs';
+import { returnConfigFileName,
+    concatenatePathAndName,
+    computeNameDepth,
+    popFirstNameElementFromName,
+    popLastPathElementFromPath } from './path_tools';
 
 // Visual settings for the viewer
 let settings_dict = {
@@ -675,54 +680,6 @@ function createBlock(element, parent, path) {
 
 function addElementToElement(block_to_add, to_block) {
     to_block.embed(block_to_add);
-}
-
-function returnConfigFileName(string) {
-    if (string) {
-        const [file, module] = string.split(":");
-        return {"file": file, "module": module}
-    }
-    else return null;
-}
-
-function concatenatePathAndName(path, name) {
-    if (path == null) {
-        return name + ':'
-    }
-    else if (path.slice(-1) == ':') {
-        return path + name;
-    }
-    else {
-        return path + '.' + name;
-    }
-}
-
-// This function does not support complete paths
-// Only names that are separated by a .
-function computeNameDepth(path) {
-    let name_list = path.split(".");
-    return name_list.length;
-}
-
-function popFirstNameElementFromName(name) {
-    // Split the blocks
-    const blocks = name.split(".");
-    const remaining_blocks = blocks.slice(1, blocks.length);
-    const remaining_name = remaining_blocks.join('.');
-    const pop = blocks[0];
-    return {'pop': pop, 'remaining': remaining_name};
-}
-
-function popLastPathElementFromPath(path) {
-    // Split the file name and the blocks
-    const file_block = path.split(":");
-    const file = file_block[0];
-    // Split the blocks
-    const blocks = file_block[1].split(".");
-    const path_blocks = blocks.slice(0, blocks.length - 1);
-    const remaining_path = file + ':' + path_blocks.join('.');
-    const name = blocks[blocks.length - 1];
-    return {'file': file, 'path': remaining_path, 'name': name};
 }
 
 function applyParentConfig(element, child_attrs) {
