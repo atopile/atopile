b7d4c75f66509699f2210d3ab594015d8607616b
Timothee Peter <timothee.peter@mac.com>
2023-08-16 15:35:45 +0000

    First stab at modularization of front end

---
+++ src/viewer/utils.js
@@ -0,0 +1,26 @@
+
+const fontHeightToPxRatio = 1.6;
+const fontLengthToPxRatio = 0.7;
+
+// Definitely need to update this garbage at some point
+export function measureText(text, text_size, direction) {
+    var string = text + '';
+    var lines = string.split("\n");
+    var width = 0;
+    for (let line of lines) {
+        var length = line.length;
+        if (length > width) {
+            width = length;
+        };
+    };
+    if (direction == 'length') {
+        // divide by 3 to go from font size to pxl, will have to fix
+        return width * text_size * fontLengthToPxRatio;
+    }
+    else if (direction == 'height') {
+        return lines.length * text_size * fontHeightToPxRatio;
+    }
+    else {
+        return 0;
+    }
+};
