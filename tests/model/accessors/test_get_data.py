8dda4d515161d05970d47cc8856526eaf58af67c
Matthew Wildoer <mawildoer@gmail.com>
2023-09-24 18:23:16 -0700

    Fix unit tests considering instace vs. class vs. superclass changes

--- tests/model/accessors/test_get_data.py
+++ tests/model/accessors/test_get_data.py
@@ -1,50 +1,58 @@
 from unittest.mock import MagicMock
+
+import pytest
 from atopile.model.accessors import ModelVertexView
 
-def test_instance_str_attribute():
+
+@pytest.mark.parametrize(
+    ("path"),
+    [
+        "some_value",
+        ("some_value",),
+    ],
+)
+def test_instance_attribute(path):
     self = MagicMock()
     self.data = {"some_value": 1234}
-    assert ModelVertexView.get_data(self, "some_value") == 1234
+    assert ModelVertexView.get_data(self, path) == 1234
 
-def test_superclass_str_attribute():
+
+@pytest.mark.parametrize(
+    ("path"),
+    [
+        "some_value",
+        ("some_value",),
+    ]
+)
+def test_instance_of_attribute(path):
     self = MagicMock()
-
-    superclass = MagicMock()
-    self.superclasses = [superclass]
-
     self.data = {}
-    superclass.data = {"some_value": 1234}
+    self.is_instance = True
+    self.instance_of = MagicMock()
+    self.instance_of.data = {"some_value": 1234}
 
-    assert ModelVertexView.get_data(self, "some_value") == 1234
+    assert ModelVertexView.get_data(self, path) == 1234
 
-def test_instance_tuple_attribute():
+@pytest.mark.parametrize(
+    ("path"),
+    [
+        "some_value",
+        ("some_value",),
+    ]
+)
+def test_superclass_attribute(path):
     self = MagicMock()
-    self.data = {"some_value": 1234}
-    assert ModelVertexView.get_data(self, ("some_value",)) == 1234
-
-def test_superclass_tuple_attribute():
-    self = MagicMock()
-
-    superclass = MagicMock()
-    self.superclasses = [superclass]
-
     self.data = {}
-    superclass.data = {"some_value": 1234}
+    self.is_instance = True
+    self.instance_of = MagicMock()
+    self.instance_of.data = {}
+    self.instance_of.superclasses = [MagicMock()]
+    self.instance_of.superclasses[0].data = {"some_value": 1234}
 
-    assert ModelVertexView.get_data(self, ("some_value",)) == 1234
+    assert ModelVertexView.get_data(self, path) == 1234
+
 
 def test_instance_nested_tuple_attribute():
     self = MagicMock()
     self.data = {"path_to": {"some_value": 1234}}
-    assert ModelVertexView.get_data(self, ("path_to", "some_value")) == 1234
-
-def test_superclass_nested_tuple_attribute():
-    self = MagicMock()
-
-    superclass = MagicMock()
-    self.superclasses = [superclass]
-
-    self.data = {}
-    superclass.data = {"path_to": {"some_value": 1234}}
-
     assert ModelVertexView.get_data(self, ("path_to", "some_value")) == 1234
