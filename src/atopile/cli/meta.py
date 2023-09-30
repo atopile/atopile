419226b1d0a900dbaa21f509c7c2c5333fcbf075
Matthew Wildoer <mawildoer@gmail.com>
2023-09-30 09:37:40 -0700

    Add meta tools to print info and update atopile

--- src/atopile/cli/meta.py
+++ src/atopile/cli/meta.py
@@ -2,9 +2,11 @@ import logging
 import subprocess
 
 import click
+from rich.console import Console
+from rich.table import Table
 
-from atopile.version import get_version, is_editable_install, warn_editable_install
-from atopile.utils import get_source_project_root
+from atopile.utils import get_source_project_root, is_editable_install
+from atopile.version import get_version
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
@@ -17,9 +19,26 @@ def meta():
 
 @meta.command()
 def info():
-    version = get_version()
-    log.info(f"atopile installed: {version}")
-    warn_editable_install()
+    """
+    Print out tabulated, coloured information about the installed atopile version
+    """
+    table = Table(title="atopile Info")
+    table.add_column("Info", style="cyan")
+    table.add_column("Value", style="magenta")
+
+    # add the version
+    table.add_row("Version", str(get_version()))
+
+    # add whether it's an editable install, making it red if it is
+    table.add_row(
+        "Editable Install",
+        str(is_editable_install()),
+        style="red" if is_editable_install() else None,
+    )
+
+    # print the table
+    console = Console()
+    console.print(table)
 
 
 @meta.command()
@@ -31,10 +50,14 @@ def update():
     """
     if is_editable_install():
         log.info("Reinstalling editable version...")
-        log.warning("This won't pull the latest from the repo to avoid overwriting local changes.")
-        log.warning("This won't install any new deps to avoid changing your environment.")
+        log.warning(
+            "This won't pull the latest from the repo to avoid overwriting local changes."
+        )
+        log.warning(
+            "This won't install any new deps to avoid changing your environment."
+        )
         subprocess.run(
-            ["pip install --no-deps -e .\"[dev,test,docs]\""],
+            ['pip install --no-deps -e ."[dev,test,docs]"'],
             check=True,
             shell=True,
             cwd=str(get_source_project_root().resolve().absolute()),
