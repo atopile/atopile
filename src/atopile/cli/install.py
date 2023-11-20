9a1c0bc67cfb54c044f7c38cc861cfa5dd2fff65
napowderly <napowderly@gmail.com>
2023-11-19 17:33:35 -0800

    fixing lint errors

--- src/atopile/cli/install.py
+++ src/atopile/cli/install.py
@@ -1,22 +1,21 @@
 import logging
-import os
+import subprocess
 import sys
 from pathlib import Path
 
 import click
-import yaml
 from git import Repo
-import subprocess
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
+
 
 @click.command()
 @click.argument("component_id")
 def install(component_id: str):
     # Example of running a simple command like 'ls' on Unix or 'dir' on Windows
     # check that component id is valid, must start with a C and then be all numbers
-    if not component_id.startswith('C'):
+    if not component_id.startswith("C"):
         log.error(f"Component id {component_id} is invalid. Aborting.")
         sys.exit(1)
     if not component_id[1:].isdigit():
@@ -24,7 +23,7 @@ def install(component_id: str):
         sys.exit(1)
 
     # Get the top level of the git module the user is currently within
-    repo = Repo('.', search_parent_directories=True)
+    repo = Repo(".", search_parent_directories=True)
     top_level_path = Path(repo.working_tree_dir)
 
     # Get the remote URL
@@ -36,15 +35,20 @@ def install(component_id: str):
     else:
         footprints_dir = top_level_path / "elec/lib/lib"
 
-
     log.info(f"Footprints directory: {footprints_dir}")
 
-    command = ["easyeda2kicad", "--full", f"--lcsc_id={component_id}", f"--output={footprints_dir}", "--overwrite"]
+    command = [
+        "easyeda2kicad",
+        "--full",
+        f"--lcsc_id={component_id}",
+        f"--output={footprints_dir}",
+        "--overwrite",
+    ]
     result = subprocess.run(command, capture_output=True, text=True)
 
     # The stdout and stderr are captured due to 'capture_output=True'
-    print("STDOUT:", result.stdout)
-    print("STDERR:", result.stderr)
+    # print("STDOUT:", result.stdout)
+    # print("STDERR:", result.stderr)
 
     # Check the return code to see if the command was successful
     if result.returncode == 0:
