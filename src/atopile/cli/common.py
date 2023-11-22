e0e773587ef0276b8b128217279247d80e2234e4
Matthew Wildoer <mawildoer@gmail.com>
2023-11-22 11:43:02 -0800

    Upgrade common build commands parser to use updated configs

--- src/atopile/cli/common.py
+++ src/atopile/cli/common.py
@@ -2,10 +2,13 @@ import functools
 import logging
 import sys
 from pathlib import Path
+from typing import Iterable
 
 import click
+from omegaconf import OmegaConf
 
-from atopile.project.config import BuildConfig, CustomBuildConfig
+from atopile.address import AddrStr, AddrValueError
+from atopile.project.config import Config
 from atopile.project.project import Project
 from atopile.version import check_project_version
 
@@ -19,101 +22,79 @@ def ingest_config_hat(f):
     # since we always need the project to figure out the config, we may as well decorate the command ourselves,
     # process things in the right order and hand them back as kw_args
 
-    @click.argument("source", required=False, default=None)
-    @click.option("--build-config", default=None)
-    @click.option("--root-file", help="DEPRECATED - do not use", default=None)
-    @click.option("--root-node", help="DEPRECATED - do not use", default=None)
+    @click.argument("entry", required=False, default=None)
+    @click.option("--build", default=None)
+    @click.option("-c", "--config", multiple=True)
     @functools.wraps(f)
     def wrapper(
         *args,
-        source: str,
-        build_config: str,
-        root_file: str,
-        root_node: str,
+        entry: str,
+        build: str,
+        config: Iterable[str],
         **kwargs,
     ):
-        if source is None:
-            source_path = Path.cwd()
-            module_path = None
-        else:
-            split_source = source.split(":")
-            if len(split_source) == 2:
-                raw_source_path, module_path = split_source
-            elif len(split_source) == 1:
-                raw_source_path = split_source[0]
-                module_path = None
-            else:
+        # basic the entry address if provided, otherwise leave it as None
+        if entry is not None:
+            try:
+                entry = AddrStr(entry)
+            except AddrValueError as ex:
                 raise click.BadParameter(
-                    f"Could not parse source path {source}. Expected format is `path/to/source.ato:module/path`."
+                    f"Invalid entry address {entry}.",
+                    param_hint="entry",
+                ) from ex
+
+            if entry.file is None:
+                raise click.BadParameter(
+                    f"Invalid entry address {entry} - entry must specify a file.",
+                    param_hint="entry",
                 )
 
-            source_path = Path(raw_source_path)
-
-            if not source_path.exists():
-                raise click.BadParameter(f"Path not found {str(source_path)}.")
-
+        # get the project
         try:
-            project: Project = Project.from_path(source_path)
+            project: Project = Project.from_path(entry.file if entry else Path.cwd())
         except FileNotFoundError as ex:
             raise click.BadParameter(
-                f"Could not find project from path {str(source_path)}. Is this file path within a project?"
+                f"Could not find project from path {str(entry.file)}. Is this file path within a project?"
             ) from ex
 
         log.info("Using project %s", project.root)
 
-        # FIXME: remove deprecated options
-        if root_file is not None or root_node is not None:
-            log.warning("Specifying root-file or root-node via options is deprecated.")
-            log.warning("... because it was a daft idea. Matt's sorry.")
-            log.warning(
-                "Please instead specify what you are pointing to with a positional argument eg."
-            )
-            log.warning(
-                "ato ... %s/%s:%s",
-                project.root.relative_to(Path(".").resolve().absolute()),
-                root_file,
-                root_node,
-            )
-            log.warning("See `atopile view --help` for more information.")
+        # set the build config
+        if build is None:
+            if build not in project.config.builds:
+                raise click.BadParameter(
+                    f'Could not find build-config "{build}". Available build configs are: {", ".join(project.config.builds.keys())}.'
+                )
+            project.config.selected_build_name = build
 
-        if module_path or source_path.is_file():
-            root_file_path = project.standardise_import_path(source_path)
-            root_node_path = str(root_file_path) + ":" + module_path
+        # add custom config overrides
+        if config:
+            cli_conf = OmegaConf.from_dotlist(config)
         else:
-            root_file_path = None
-            root_node_path = None
+            cli_conf = OmegaConf.create()
 
-        if build_config is None:
-            base_build_config_obj: BuildConfig = project.config.builds["default"]
-        else:
-            if build_config in project.config.builds:
-                base_build_config_obj = project.config.builds[build_config]
+        # finally smoosh them all back together like a delicious cake
+        # FIXME: why are we smooshing this -> does this need to be mutable?
+        project.config: Config = OmegaConf.merge(project.config, cli_conf)
+
+        # layer on the selected addrs config
+        if entry and entry.file.is_file():
+            # NOTE: we already check that entry.file isn't None if entry is specified
+            if entry.node:
+                std_entry_file = project.standardise_import_path(entry.file.expanduser().resolve().absolute())
+                project.config.selected_build.entry = AddrStr.from_parts(std_entry_file, entry.node)
             else:
                 raise click.BadParameter(
-                    f'Could not find build-config "{build_config}".'
+                    "If an entry of a file is specified, you must specify"
+                    f" the node within it you want to build {entry}.",
+                    param_hint="entry",
                 )
-        if root_file_path is not None:
-            build_config_obj = CustomBuildConfig.from_build_config(
-                base_build_config_obj
-            )
-            build_config_obj.root_file = (
-                (project.root / root_file_path).resolve().absolute()
-            )
-            if root_node_path is not None:
-                build_config_obj.root_node = root_node_path
-        else:
-            if root_node_path is not None:
-                raise click.BadParameter(
-                    "Cannot specify root-node without specifying root-file via positional argument."
-                )
-            build_config_obj = base_build_config_obj
-        log.info("Using build config %s", build_config_obj.name)
 
         # perform pre-build checks
         if not check_project_version(project):
             sys.exit(1)
 
         # do the thing
-        return f(*args, project, build_config_obj, **kwargs)
+        return f(*args, project, **kwargs)
 
     return wrapper
