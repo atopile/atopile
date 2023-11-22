2f3820435cb841c9fba70b1aaafa8183b17c2ef0
Matthew Wildoer <mawildoer@gmail.com>
2023-11-21 22:59:29 -0800

    Rebuild a better configs module using OmegaConf

--- src/atopile/project/config.py
+++ src/atopile/project/config.py
@@ -1,6 +1,11 @@
+import collections.abc
 import logging
 from pathlib import Path
-from typing import TYPE_CHECKING, Dict, List
+from typing import TYPE_CHECKING, Any, Optional
+
+import yaml
+from attrs import Factory, define
+from omegaconf import MISSING, OmegaConf
 
 if TYPE_CHECKING:
     # See: https://adamj.eu/tech/2021/05/13/python-type-hints-how-to-fix-circular-imports/
@@ -10,130 +15,74 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
 
-class BaseConfig:
-    def __init__(self, config_data: dict, project: "Project", name=None) -> None:
-        self._config_data = config_data
-        self.project = project
-        self._name = name
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @classmethod
-    def from_config(cls, config: "BaseConfig") -> "BaseConfig":
-        return cls(config._config_data, config.project, config.name)
-
-    def _return_subconfig(self, key: str, return_type) -> "BaseConfig":
-        return return_type(self._config_data.get(key, {}), self.project, name=key)
-
-    def _return_list_of(self, list_key: str, return_type) -> list:
-        return [
-            return_type(d, self.project) for d in self._config_data.get(list_key, [])
-        ]
-
-    def _return_dict_of(self, dict_key: str, return_type) -> dict:
-        return {
-            k: return_type(v, self.project, name=k)
-            for k, v in self._config_data.get(dict_key, {}).items()
-        }
+USER_CONFIG_PATH = Path("~/.atopile/config.yaml").expanduser().resolve().absolute()
 
 
-class Config(BaseConfig):
-    @property
-    def paths(self) -> "Paths":
-        return self._return_subconfig("paths", Paths)
+def _sanitise_key(s: str) -> str:
+    return s.replace("-", "_")
 
-    @property
-    def atopile_version(self) -> str:
-        return self._config_data.get("ato-version")
+def _sanitise_item(item: tuple[Any, Any]) -> tuple[Any, Any]:
+    k, v = item
+    if isinstance(v, collections.abc.Mapping):
+        return _sanitise_key(k), _sanitise_dict_keys(v)
+    return _sanitise_key(k), v
 
-    @property
-    def builds(self) -> Dict[str, "BuildConfig"]:
-        build_dict = self._return_dict_of("builds", BuildConfig)
-        if "default" not in build_dict:
-            build_dict["default"] = BuildConfig("default", {}, self.project)
-        return build_dict
-
-    @property
-    def targets(self) -> Dict[str, "BaseConfig"]:
-        return self._return_dict_of("targets", BaseConfig)
+def _sanitise_dict_keys(d: collections.abc.Mapping) -> collections.abc.Mapping:
+    return dict(_sanitise_item(item) for item in d.items())
 
 
-class Paths(BaseConfig):
-    @property
-    def build(self) -> Path:
-        build_dir = self._config_data.get("build")
-        if build_dir is None:
-            return (self.project.root / "build").resolve().absolute()
+@define
+class Paths:
+    project: Path = MISSING  # should be the absolute path to the project root
 
-        build_dir = Path(build_dir)
-        if not build_dir.is_absolute():
-            build_dir = self.project.root / build_dir
+    build: Path = "build"
+    abs_build: Path = "${.project}/${.build}"
 
-        return build_dir.resolve().absolute()
-
-    @property
-    def lib_path(self) -> Path:
-        lib_path: str = self._config_data.get("lib-path", "../lib/lib.pretty")
-        return (self.project.root / lib_path).resolve().absolute()
-
-class BuildConfig(BaseConfig):
-    @property
-    def default(self) -> "BuildConfig":
-        return self.project.config.builds["default"]
-
-    @property
-    def root_file(self) -> Path:
-        if "root-file" not in self._config_data:
-            return
-        return self.project.root / self._config_data["root-file"]
-
-    @property
-    def root_node(self) -> str:
-        return self._config_data.get("root-node", self._config_data.get("root-file"))
-
-    @property
-    def targets(self) -> List[str]:
-        return self._config_data.get("targets") or [
-            "designators",
-            "netlist-kicad6",
-            "bom-jlcpcb",
-            "kicad-lib-paths"
-        ]
-
-    @property
-    def build_path(self) -> Path:
-        return self.project.config.paths.build / self.name
+    footprints: Path = "../lib/lib.pretty"
+    abs_footprints: Path = "${.project}/${.footprints}"
 
 
-class CustomBuildConfig:
-    def __init__(
-        self, name: str, project: "Project", root_file, root_node, targets, config_data
-    ) -> None:
-        self._name = name
-        self.project = project
-        self.root_file = root_file
-        self.root_node = root_node
-        self.targets = targets
-        self._config_data = config_data
+@define
+class BuildConfig:
+    entry: str = MISSING
+
+    targets: list[str] = ["designators", "netlist-kicad6", "bom-jlcpcb", "kicad-lib-paths"]
+    build_path: Path = "${..paths.abs_build}/${.name}"
 
 
-    @property
-    def name(self) -> str:
-        return self._name
+@define
+class Config:
+    paths: Paths = Factory(Paths)
 
-    @staticmethod
-    def from_build_config(build_config: BuildConfig) -> "CustomBuildConfig":
-        return CustomBuildConfig(
-            name=build_config.name,
-            project=build_config.project,
-            root_file=build_config.root_file,
-            root_node=build_config.root_node,
-            targets=build_config.targets,
-            config_data=build_config._config_data,
-        )
+    ato_version: str = "^0.0.0"
 
-    @property
-    def build_path(self) -> Path:
-        return self.project.config.paths.build / self.name
+    builds: dict[str, BuildConfig] = Factory(lambda: {"default": BuildConfig()})
+    default_build: BuildConfig = "${.builds[default]}"
+
+    selected_build_config_name: str = "default"
+    selected_build_config: BuildConfig = "${.builds[${.selected_build_config_name}]}"
+
+
+def make_config(project_config: Path, build_config: str = "default") -> Config:
+    """
+    Make a config object for a project.
+
+    The typing on this is a little white lie... because they're really OmegaConf objects.
+    """
+    structure = Config()
+    structure.paths.project = project_config
+    structure.selected_build_config_name = build_config
+
+    if USER_CONFIG_PATH.exists():
+        user_config = OmegaConf.load(USER_CONFIG_PATH)
+    else:
+        user_config = OmegaConf.create()  # empty config
+
+    with project_config.open() as f:
+        config_data = yaml.safe_load(f)
+
+    return OmegaConf.merge(
+        OmegaConf.structured(structure),  # structure
+        user_config,  # user config
+        OmegaConf.create(_sanitise_dict_keys(config_data)),  # project config
+    )
