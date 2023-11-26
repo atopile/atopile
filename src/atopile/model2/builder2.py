eb8e3aae43eb7cc100cbb3711ae4ef1753b4955d
Matthew Wildoer <mawildoer@gmail.com>
2023-11-25 17:13:03 -0800

    Add temp lookup methods to builder2

--- src/atopile/model2/builder2.py
+++ src/atopile/model2/builder2.py
@@ -12,14 +12,16 @@ from .datatypes import Ref
 from .parse_utils import get_src_info_from_ctx
 
 
-def lookup_filename(cwd: str, filename: str) -> str:
-    """Placeholder"""
-    raise NotImplementedError
+SEARCH_PATHS = (
+    Path("/Users/mattwildoer/Projects/atopile-workspace/skate-board-brake-light/elec/src"),
+    Path("/Users/mattwildoer/Projects/atopile-workspace/skate-board-brake-light/elec/src/.ato/modules"),
+    Path("/Users/mattwildoer/Projects/atopile-workspace/skate-board-brake-light/elec/src/.ato/modules/modules"),
+)
 
 
 def lookup_ref(obj: Object, ref: Ref) -> Object:
     """Basic ref lookup"""
-    if ref.count() != 1:
+    if len(ref) != 1:
         raise NotImplementedError
 
     if ref in obj.named_locals:
@@ -51,27 +53,48 @@ class Lofty:
         self.error_handler = error_handler
         self.paths_to_objs = paths_to_objs
 
+    def lookup_filename(self, cwd: str | Path, from_name: str) -> Path:
+        """Look up a filename from a from_name."""
+        cwd = Path(cwd)
+        if cwd.is_file():
+            cwd = cwd.parent
+
+        for search_path in (cwd,) + SEARCH_PATHS:
+            if (search_path / from_name) in self.paths_to_objs:
+                return search_path / from_name
+
+        raise FileNotFoundError
+
     def visit_object(self, obj: Object) -> None:
         """Visit and resolve imports in an object."""
-        for imp in obj.locals_by_type[Import]:
+        for _, imp in obj.locals_by_type[Import]:
             assert isinstance(imp, Import)
             self.visit_import(imp)
 
-        for next_obj in obj.locals_by_type[Object]:
+        for _, next_obj in obj.locals_by_type[Object]:
             self.visit_object(next_obj)
 
     def visit_import(self, imp: Import) -> None:
         """Visit and resolve an import."""
         assert imp.src_ctx is not None
         cwd, _, _ = get_src_info_from_ctx(imp.src_ctx)
-        foreign_filename = lookup_filename(cwd, imp.from_name)
+
+        try:
+            foreign_filename = self.lookup_filename(cwd, imp.from_name)
+        except FileNotFoundError:
+            self.error_handler.handle(
+                errors.AtoImportNotFoundError.from_ctx(
+                    f"File '{imp.from_name}' not found.", imp.src_ctx
+                )
+            )
+            return
 
         try:
             foreign_root = self.paths_to_objs[foreign_filename]
         except KeyError:
             self.error_handler.handle(
                 errors.AtoImportNotFoundError.from_ctx(
-                    f"File '{foreign_filename}' not found.", imp.src_ctx
+                    f"File '{foreign_filename}' not parsed.", imp.src_ctx
                 )
             )
             return
