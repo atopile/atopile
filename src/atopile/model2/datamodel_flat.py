2bdd5f57383fc23d77b6f79477fe63fb1e05a1e6
Matthew Wildoer <mawildoer@gmail.com>
2023-11-27 14:21:29 -0800

    Basis for flattened data-structure

---
+++ src/atopile/model2/datamodel_flat.py
@@ -0,0 +1,38 @@
+"""
+This datamodel represents the circuit from a specific point of view - eg. through a certain path.
+It's entirely invalidated when the circuit changes at all and needs to be rebuilt.
+"""
+import logging
+from typing import Optional
+
+from attrs import define, resolve_types
+
+from . import datamodel as dm1
+
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+
+
+@define
+class Link:
+    """Represent a connection between two connectable things."""
+    origin_link: dm1.Link
+    origin_instance: "Instance"
+    source: "Instance"
+    target: "Instance"
+
+
+@define
+class Instance:
+    """Represent a concrete object class."""
+    path: tuple[str]
+    class_: tuple[dm1.Object]
+    parent: Optional["Instance"]
+    children: dict[str, "Instance"]
+    links_: tuple[Link]
+
+
+resolve_types(Link)
+resolve_types(Instance)
