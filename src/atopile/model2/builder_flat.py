1a3ddd6d9e88c4208516edf3a94e840e7dd5cd17
Matthew Wildoer <mawildoer@gmail.com>
2023-11-27 22:33:58 -0800

    Remove Muck - it was only one function

--- src/atopile/model2/builder_flat.py
+++ src/atopile/model2/builder_flat.py
@@ -16,59 +16,55 @@ def get_ref_from_instance(ref: Ref, instance: Instance) -> Instance:
 
 
 def build(obj: dm1.Object) -> Instance:
-    """Build the model."""
-    muck = Muck()
-    return muck.visit_object((), obj)
+    """Build a flat datamodel."""
+    return _build((), obj)
 
 
-class Muck:
-    """Muck's job is to walk through the tree and build a flat model from it."""
+def _build(addr: Ref, obj: dm1.Object, instance: Optional[Instance] = None) -> Instance:
+    """Visit an object."""
+    if instance and obj in instance.origin.supers_bfs:
+        return instance
+    elif obj.supers_bfs:
+        instance = _build(addr, obj.supers_bfs[0], instance)
+    else:
+        instance = Instance(addr=addr)
 
-    def visit_object(self, addr: Ref, obj: dm1.Object, instance: Optional[Instance] = None) -> Instance:
-        """Visit an object."""
-        if instance and obj in instance.origin.supers_bfs:
-            return instance
-        elif obj.supers_bfs:
-            instance = self.visit_object(addr, obj.supers_bfs[0], instance)
-        else:
-            instance = Instance(addr=addr)
+    instance.origin = obj
 
-        instance.origin = obj
+    # visit all the child objects
+    child_objects = obj.locals_by_type[dm1.Object]
+    instance.children_from_classes.update(
+        {ref[0]: _build(addr + ref, value) for ref, value in child_objects}
+    )
 
-        # visit all the child objects
-        child_objects = obj.locals_by_type[dm1.Object]
-        instance.children_from_classes.update(
-            {ref[0]: self.visit_object(addr + ref, value) for ref, value in child_objects}
+    # visit replacements after the children are created
+    replacements = obj.locals_by_type[dm1.Replace]
+    for _, replace in sorted(replacements, key=lambda x: len(x[1].original_ref)):
+        assert isinstance(replace, dm1.Replace)
+        instance_to_replace = get_ref_from_instance(replace.original_ref, instance)
+        _build(
+            instance_to_replace.addr,
+            replace.replacement_obj,
+            instance_to_replace
         )
 
-        # visit replacements after the children are created
-        replacements = obj.locals_by_type[dm1.Replace]
-        for _, replace in sorted(replacements, key=lambda x: len(x[1].original_ref)):
-            assert isinstance(replace, dm1.Replace)
-            instance_to_replace = get_ref_from_instance(replace.original_ref, instance)
-            self.visit_object(
-                instance_to_replace.addr,
-                replace.replacement_obj,
-                instance_to_replace
-            )
+    # visit all the child params
+    params = obj.locals_by_type[(str, int)]
+    for ref, value in params:
+        to_mod = get_ref_from_instance(ref[:-1], instance)
+        to_mod.children[ref[-1]] = value
 
-        # visit all the child params
-        params = obj.locals_by_type[(str, int)]
-        for ref, value in params:
-            to_mod = get_ref_from_instance(ref[:-1], instance)
-            to_mod.children[ref[-1]] = value
+    # visit all the child links
+    links = obj.locals_by_type[dm1.Link]
+    for _, link in links:
+        assert isinstance(link, dm1.Link)
+        source_instance = get_ref_from_instance(link.source_ref, instance)
+        target_instance = get_ref_from_instance(link.target_ref, instance)
 
-        # visit all the child links
-        links = obj.locals_by_type[dm1.Link]
-        for _, link in links:
-            assert isinstance(link, dm1.Link)
-            source_instance = get_ref_from_instance(link.source_ref, instance)
-            target_instance = get_ref_from_instance(link.target_ref, instance)
+        flat_link = Link(link, instance, source_instance, target_instance)
 
-            flat_link = Link(link, instance, source_instance, target_instance)
+        source_instance.linked_to_me.append(flat_link)
+        target_instance.linked_to_me.append(flat_link)
+        instance.links.append(flat_link)
 
-            source_instance.linked_to_me.append(flat_link)
-            target_instance.linked_to_me.append(flat_link)
-            instance.links.append(flat_link)
-
-        return instance
+    return instance
