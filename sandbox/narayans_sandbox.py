f72ffe316701fbacc3c61a2e17064a754e275d5a
napowderly <napowderly@gmail.com>
2023-11-30 16:38:44 -0800

    dont worry about it

--- sandbox/narayans_sandbox.py
+++ sandbox/narayans_sandbox.py
@@ -10,7 +10,7 @@ from atopile.address import AddrStr
 from atopile.model2.build import Spud
 from atopile.model2.datamodel import Instance
 from atopile.model2.errors import ErrorHandler, HandlerMode
-from atopile.model2.net_naming import generate_base_net_name, resolve_name_conflicts
+from atopile.model2.net_naming import find_net_names
 from atopile.model2.instance_methods import iter_nets
 
 from datamodel1 import make_tree, print_tree, src_code
@@ -23,24 +23,10 @@ spud = Spud(error_handler, (Path("."),))
 flat = spud.build_instance_from_text(dedent(src_code).strip(), ("Root",))
 print_tree(make_tree(flat))
 # %%
-nets = list(list(net) for net in iter_nets(flat))
-# %%
-# print out the nets refs
-for net in nets:
-    for instance in net:
-        print(instance.ref)
+nets = find_net_names(iter_nets(flat))
 
-# %% Generate net names
-counter = count(1)
-names = {}
 for net in nets:
-    name = generate_base_net_name(net)
-    names[name] = net
-    print(name.name)
+    print(net.get_name())
+    # print_tree(make_tree(net))
 # %%
-resolve_name_conflicts(names)
-# %%
-# print out the nets names
-for name, net in names.items():
-    print(name)
-# %%
+
