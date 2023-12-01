f7df2d65130c5bcf7014a7f841d58841b1a8a0e0
napowderly <napowderly@gmail.com>
2023-11-30 16:38:15 -0800

    net name collisions resolved

--- src/atopile/model2/net_naming.py
+++ src/atopile/model2/net_naming.py
@@ -3,117 +3,114 @@ Net naming algorithm.
 """
 import logging
 from collections import ChainMap, defaultdict
-from typing import Any, Callable, Iterable, Iterator, Optional, Hashable, List, Tuple
+from typing import Any, Callable, Hashable, Iterable, Iterator, List, Optional, Tuple
 
 from attrs import define, field, resolve_types
 
 from atopile.model2 import datamodel as dm1
-from atopile.model2.datatypes import Ref, KeyOptItem
 from atopile.model2.datamodel import Instance, Joint
-from atopile.model2.instance_methods import lowest_common_parent, match_signals
+from atopile.model2.datatypes import KeyOptItem, Ref
+from atopile.model2.instance_methods import (
+    am_in_interface,
+    iter_parents,
+    lowest_common_parent,
+    match_interfaces,
+    match_signals,
+)
 
+from toolz import groupby
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
 # create a Net class that holds the name and the instances
+@define
 class Net:
-    def __init__(self,name: str, instances: list) -> None:
-        self.name = name
-        self.instances = instances
+    nodes_on_net: list[Instance]
 
-def am_in_interface(instance: Instance) -> bool:
-    """Am I in an interface?"""
-    # check if any supers are interfaces
-    # return True if so
-    # return False if not
-    return any(map(lambda obj: obj.origin is dm1.INTERFACE, instance.origin.supers_bfs))
+    base_name: Optional[str] = None
 
-def generate_base_net_name(net_objects: Iterable[Instance]) -> Net:
-    WEIGHT_NO_GRANDPARENTS = 10
-    WEIGHT_INTERFACE_GRANDPARENT = 5
-    WEIGHT_SIGNAL = 2
-    name_candidates = defaultdict(int)
-    signals = filter(match_signals, net_objects)
+    lcp: Optional[Instance] = None
+    prefix: Optional[Ref] = None
 
-    for signal in signals:
-        name = signal.ref[-1]
-        try:
+    suffix: Optional[int] = None
+
+    def get_name(self) -> str:
+        """Get the name of the net."""
+        return (f"{self.prefix + '_' if self.prefix else ''}"
+                f"{self.base_name or 'NET'}"
+                f"{'_' + str(self.suffix) if self.suffix else ''}")
+
+    def generate_base_net_name(self) -> Optional[str]:
+        """TODO:"""
+        WEIGHT_NO_GRANDPARENTS = 10
+        WEIGHT_INTERFACE_GRANDPARENT = 5
+        WEIGHT_SIGNAL = 2
+
+        name_candidates = defaultdict(int)
+
+        for signal in filter(match_signals, self.nodes_on_net):
+            name = signal.ref[-1]
             if signal.parent is None:
                 name_candidates[name] += WEIGHT_NO_GRANDPARENTS
             elif am_in_interface(signal):
                 name_candidates[name] += WEIGHT_INTERFACE_GRANDPARENT
             else:
                 name_candidates[name] += WEIGHT_SIGNAL
-        except AttributeError:
-            name_candidates[name] += WEIGHT_NO_GRANDPARENTS
 
-    if name_candidates:
-        # print(name_candidates)
-        name = max(name_candidates, key=name_candidates.get)
-    else:
-        name = f"Net"
+        if name_candidates:
+            highest_rated_name = max(name_candidates, key=name_candidates.get)
+            self.base_name = highest_rated_name
 
-    return Net(name, list(net_objects))
 
-def remove_common_prefix(ref: Ref, prefix: Ref) -> Ref:
-    """Remove the common prefix from a ref."""
-    if len(prefix) > len(ref):
-        raise ValueError("Prefix is longer than ref.")
+def find_net_names(nets: Iterable[Iterable[Instance]]) -> dict[str, list[Instance]]:
+    """Find the names of the nets."""
+    # make net objects
+    nets = [Net(list(net)) for net in nets]
+    # grab all the nets base names
+    [net.generate_base_net_name() for net in nets]
+    # for the net objects that still conflict, grab a prefix
+    conflicing_nets = find_conflicts(nets)
+    add_prefix(conflicing_nets)
+    conflicing_nets = find_conflicts(nets)
+    add_suffix(conflicing_nets)
+    return nets
 
-    for i in range(len(prefix)):
-        if prefix[i] != ref[i]:
-            raise ValueError("Prefix does not match ref.")
+    # check again for conflicts
+    conflicing_nets = find_conflicts(nets)
+    fuck_it_slap_some_numbers_on_it(conflicing_nets)
 
-    return ref[len(prefix):]
 
-def find_conflicts(nets: List[Net]) -> Tuple[List[Net], List[Net]]:
-    """Find conflicts in net names."""
-    conflicts = []
-    no_conflicts = []
-    used_names = {}
+def fuck_it_slap_some_numbers_on_it(nets: Iterable[Net]):
+    raise NotImplementedError
 
-    for net in nets:
-        if net.name in used_names:
-            # If the name is already in use, add both the current net and the
-            # net that's already using the name to conflicts
-            conflicts.append(net)
-            if used_names[net.name] not in conflicts:
-                conflicts.append(used_names[net.name])
-        else:
-            # If the name is not in use, add the net to no_conflicts and record
-            # that the name is now in use
-            used_names[net.name] = net
+def find_conflicts(nets: Iterable[Net]) -> Iterable[Iterable[Net]]:
+    """"""
+    nets_grouped_by_name = groupby(lambda net: net.get_name(), nets)
+    for nets in nets_grouped_by_name.values():
+        if len(nets) > 1:
+            yield nets
 
-    # Remove conflicting nets from no_conflicts
-    no_conflicts = [net for net in no_conflicts if net not in conflicts]
 
-    return conflicts, no_conflicts
-
-def resolve_conflicts(conflicts: List[Net]) -> List[Net]:
+def add_prefix(conflicts: Iterator[list[Net]]):
     """Resolve conflicts in net names."""
-    resolved_nets = []
-    for net in conflicts:
-        common_parent = lowest_common_parent(net.instances)
-        for instance in net.instances:
-            new_name = remove_common_prefix(instance.ref, common_parent.ref)
-            resolved_nets.append(Net(new_name, [instance]))
-    return resolved_nets
+    # Find lcp for each conflicting set of net names
+    # for each list of conflicts, get the instances from the net
+    for conflict_nets in conflicts:
+        for i, net in enumerate(conflict_nets):
+            if net.base_name:
+                net.prefix = net.nodes_on_net[0].ref[0]
 
-def ensure_uniqueness(nets: List[Net]) -> List[Net]:
-    """Ensure all net names are unique by appending integers to duplicates."""
-    final_conflicts, no_conflicts = find_conflicts(nets)
-    unique_nets = []
-    for net in final_conflicts:
-        if len(net.instances) > 1:
-            for i, instance in enumerate(net.instances):
-                unique_nets.append(Net(f"{net.name}_{i+1}", [instance]))
-        else:
-            unique_nets.append(net)
-    return unique_nets + no_conflicts
+def add_suffix(conflicts: Iterator[list[Net]]):
+    """Add an integer suffix to the nets to resolve conflicts."""
+    for conflict_nets in conflicts:
+        for i, net in enumerate(conflict_nets):
+            net.suffix = i
 
-def resolve_name_conflicts(nets: List[Net]) -> List[Net]:
-    """Resolve name conflicts."""
-    conflicts, no_conflicts = find_conflicts(nets)
-    resolved_nets = resolve_conflicts(conflicts)
-    return ensure_uniqueness(resolved_nets + no_conflicts)
+
+def remove_lcp_from_ref(ref: Ref, lcp) -> Ref:
+    """
+    Remove the lcp from the reference and return a new Ref object.
+    """
+    # Assuming that the lcp is always a part of the reference and needs to be removed
+    return Ref(tuple(part for part in ref if part != lcp))
