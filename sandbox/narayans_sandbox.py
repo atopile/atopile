20dfee4db9da8b1cecfaefe6ac8a8dd4230e5832
napowderly <napowderly@gmail.com>
2023-11-30 17:44:39 -0800

    dont worry about it

--- sandbox/narayans_sandbox.py
+++ sandbox/narayans_sandbox.py
@@ -13,7 +13,55 @@ from atopile.model2.errors import ErrorHandler, HandlerMode
 from atopile.model2.net_naming import find_net_names
 from atopile.model2.instance_methods import iter_nets
 
-from datamodel1 import make_tree, print_tree, src_code
+from datamodel1 import make_tree, print_tree
+
+
+#%%
+src_code = """
+    interface Power:
+        signal vcc
+        signal gnd
+
+    component Resistor:
+        pin p1
+        pin p2
+
+    module Root:
+        r1 = new Resistor
+        power = new Power
+        r1.p1 ~ power.vcc
+        r1.p2 ~ power.gnd
+
+        vdiv = new VDiv
+        vdiv2 = new VDiv
+
+        pin p1
+        pin p2
+        pin p3
+        pin p4
+
+        p1 ~ p2
+
+        nested = new NestedVdiv
+        double_nested = new TwoLayerNestedVdiv
+
+    module NestedVdiv:
+        vdiv = new VDiv
+
+    module TwoLayerNestedVdiv:
+        nested = new NestedVdiv
+    module VDiv:
+        r_top = new Resistor
+        r_bottom = new Resistor
+
+        signal top ~ r_top.p1
+        signal output ~ r_top.p2
+        output ~ r_bottom.p1
+        signal bottom ~ r_bottom.p2
+
+        r_top.value = 1000
+
+"""
 
 #%% Create a simple netlist
 error_handler = ErrorHandler(handel_mode=HandlerMode.RAISE_ALL)
