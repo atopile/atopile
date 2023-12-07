8829348ccc450775eee50f4871616b047e9da0a1
Timot05 <timothee.peter@mac.com>
2023-12-06 17:15:18 -0800

    add get children

--- src/atopile/build.py
+++ src/atopile/build.py
@@ -71,9 +71,21 @@ _children: dict[str, list[str]] = {
     ":Root::vdiv.power.gnd": [],
 }
 
-def get_children(addr: str) -> list[str]:
-    # TODO: write me irl
-    return _children[addr]
+# def get_children(addr: str) -> list[str]:
+#     # TODO: write me irl
+#     return _children[addr]
+
+def get_children(address: str) -> Iterable[Instance]:
+    root_addr = get_entry(address)
+    root_instance = lofty[root_addr]
+    ref_str = get_ref(address)
+    for child_ref in ref_str:
+        nested_instance = root_instance.children[child_ref]
+    children_to_return = {}
+    for child_key, child_to_return in nested_instance.children.items():
+        children_to_return[address + child_key] = child_to_return #TODO: might want to add a function to append two strings together
+
+    return children_to_return
 
 _data: dict[str, dict[str, Any]] = {
     ":Root::": {},
