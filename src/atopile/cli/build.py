df6ea1d79829f6f989fd1781b3b130a2da4d7748
Matthew Wildoer <mawildoer@gmail.com>
2023-12-06 17:16:05 -0800

    Refactor the build CLI command a little

--- src/atopile/cli/build.py
+++ src/atopile/cli/build.py
@@ -2,13 +2,22 @@
 
 import logging
 import sys
+from itertools import chain
 from pathlib import Path
-from omegaconf import OmegaConf
-from atopile.config import Config
+from textwrap import dedent
 
 import click
+import rich
+import rich.tree
+from omegaconf import OmegaConf
 
+from atopile import address
+from atopile.address import AddrStr
 from atopile.cli.common import project_options
+from atopile.config import Config
+from atopile.errors import ErrorHandler, HandlerMode
+from atopile.front_end import Dizzy, Instance, Lofty, Scoop
+from atopile.parse import FileParser
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
@@ -28,23 +37,31 @@ def build(
     Specify the root source file with the argument SOURCE.
     eg. `ato build --target my_target path/to/source.ato:module.path`
     """
-    log.info("Writing build output to %s", config)
     # input sanitisation
     # if debug:
     #     import atopile.parser.parser  # pylint: disable=import-outside-toplevel
-
     #     atopile.parser.parser.log.setLevel(logging.DEBUG)
 
     if strict is None:
         strict = False
 
-    # build core model
-    #model = build_model(project) #TODO: initate the build process
-    exit_code = 0
 
-
-    # generate targets
-    log.info("Writing build output to %s", config.abs_build)
+    log.info("Writing build output to %s", config.paths.abs_build)
     config.paths.abs_build.mkdir(parents=True, exist_ok=True)
+
+    # Do the build
+    error_handler = ErrorHandler(handel_mode=HandlerMode.RAISE_ALL)
+
+    search_paths = [
+        Path("/Users/mattwildoer/Projects/atopile-workspace/servo-drive/elec/src/"),
+    ]
+
+    parser = FileParser()
+
+    scoop = Scoop(error_handler, parser.get_ast_from_file, search_paths)
+    dizzy = Dizzy(error_handler, scoop.get_obj_def)
+    lofty = Lofty(error_handler, dizzy.get_obj_layer)
+
+    entry_instance_tree = lofty.get_instance_tree(from_parts("/Users/mattwildoer/Projects/atopile-workspace/servo-drive/elec/src/spin_servo_nema17.ato", "SpinServoNEMA17"))
 
     
