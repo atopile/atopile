de3f094e6585b2b526c5c2c00514194d9c914314
Matthew Wildoer <mawildoer@gmail.com>
2023-12-07 17:35:48 -0800

    Add equations to parameters

--- src/atopile/parameters.py
+++ src/atopile/parameters.py
@@ -1,7 +1,11 @@
-from attr import define
-from atopile.instance_methods import get_parent, get_name
 from numbers import Number
-from atopile.instance_methods import get_data_dict
+
+from attr import define
+
+from atopile.address import AddrStr
+from atopile import address
+from atopile.instance_methods import get_data_dict, get_name, get_parent
+from atopile.equations import EquationBuilder
 
 
 @define
@@ -16,10 +20,30 @@ class Parameter:
 DEFAULT_TOLERANCE = 0.01  # 1%
 
 
+_known_values: dict[AddrStr, Number] = {}
+_eqn_builder = EquationBuilder()
+
+
 def get_parameter(addr: str) -> Parameter:
     """Return a parameter for a given address."""
     # if we know what do to solve this param, do it
     # TODO: we currently don't know how to solve for parameters
+
+    if not _known_values:
+        root = address.get_entry(addr)
+        _eqn_builder.build(root)
+
+        unknowns = []
+        for addr in _eqn_builder._symbols:
+            parent = get_parent(addr)
+            data_dict = get_data_dict(parent)
+            value = data_dict[get_name(addr)]
+            if isinstance(value, Number):
+                _known_values[addr] = value
+            else:
+                unknowns.append(addr)
+
+        _known_values.update(_eqn_builder.solve(_known_values, unknowns))
 
     # otherwise, make a parameter representing the value the user has assigned
     # first check that the addr's parent exists
@@ -30,13 +54,17 @@ def get_parameter(addr: str) -> Parameter:
     spec = get_data_dict(parent)[get_name(addr)]
 
     if spec == "UNKNOWN":
-        raise ValueError("Parameter doesn't have a known value")
-
-    if not isinstance(spec, Number):
+        try:
+            value = _known_values[addr]
+        except KeyError:
+            raise ValueError("No value available for this parameter")
+    elif isinstance(spec, Number):
+        value = spec
+    else:
         raise ValueError("Cannot make a parameter from a string")
 
     return Parameter(
-        min=spec * (1 - DEFAULT_TOLERANCE),
-        max=spec * (1 + DEFAULT_TOLERANCE),
+        min=value * (1 - DEFAULT_TOLERANCE),
+        max=value * (1 + DEFAULT_TOLERANCE),
         unit="",  # TODO:
     )
