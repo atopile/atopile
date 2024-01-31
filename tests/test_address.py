bf46d79cf74b8da03d65d45714a37d4bdff41b10
Matthew Wildoer <mawildoer@gmail.com>
2024-01-30 20:47:01 -0800

    Add proper address cases

--- tests/test_address.py
+++ tests/test_address.py
@@ -16,3 +16,14 @@ def test_get_file():
 
     # Test with an empty file address
     assert get_file("") == ""
+
+    assert get_file("path/to/file.txt:a:b.c") == "path/to/file.txt"
+
+    # Test with a file address containing special characters
+    assert (
+        get_file("path/to/file-with-special-chars.txt:a:b.c")
+        == "path/to/file-with-special-chars.txt"
+    )
+
+    # Test with a file address containing a Windows drive letter
+    assert get_file("C:/path/to/file.txt:a:b.c") == "C:/path/to/file.txt"
