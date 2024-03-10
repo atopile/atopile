fa35883b885df195f7888995b088057642e721b5
Matthew Wildoer <mawildoer@gmail.com>
2024-03-10 13:50:53 -0700

    Switch Omegaconf for cattrs, add config saving and reformat dependency structure

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,9 +1,30 @@
 from atopile import config
+from ruamel.yaml import YAML
+import copy
+
+yaml = YAML()
 
 
-def test_sanitise_dict_keys():
-    """Test that dict keys are sanitised."""
-    assert config._sanitise_dict_keys({"a-b": 1, "ato-version": {"e-f": 2}}) == {
-        "a-b": 1,
-        "ato_version": {"e-f": 2},
-    }
+def test_roundtrip():
+    config_dict = yaml.load("""
+        ato-version: ^0.2.0
+        builds:
+            debug:
+                entry: elec/src/debug.ato:Debug
+        unknown: test
+        # comments
+        dependencies:
+        - tps63020dsjr # comments
+        - usb-connectors ^v2.0.1
+        - esp32-s3:
+            version: ^v0.0.1
+            path: ../esp32-s3
+        """)
+    cfg = config.ProjectConfig.structure(config_dict)
+    assert config_dict == cfg.patch_config(config_dict)
+    assert cfg.ato_version == "^0.2.0"
+
+    cfg.ato_version = "10"
+    config_dict_2 = copy.deepcopy(config_dict)
+    config_dict_2["ato-version"] = "10"
+    assert config_dict_2 == cfg.patch_config(config_dict)
