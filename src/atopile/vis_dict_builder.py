30c2cb0ca14096c4dd26a8baffce1f0fa2cb5b91
Timot05 <timothee.peter@mac.com>
2024-03-26 17:23:32 -0700

    cleanup the dict

--- src/atopile/vis_dict_builder.py
+++ src/atopile/vis_dict_builder.py
@@ -1,4 +1,4 @@
-from atopile.address import AddrStr, get_parent_instance_addr, get_name
+from atopile.address import AddrStr, get_parent_instance_addr, get_name, get_instance_section
 from atopile.instance_methods import (
     get_children,
     get_links,
@@ -7,12 +7,12 @@ import json
 
 
 def get_vis_dict(addr: AddrStr) -> str:
-    block_list = [instance for instance in get_children(addr)]
+    block_list = [get_instance_section(instance) for instance in get_children(addr)]
     links = get_links(addr)
     link_list = []
     for link in links:
-        _source = {"block": get_parent_instance_addr(link.source.addr), "port": get_name(link.source.addr)}
-        _target = {"block": get_parent_instance_addr(link.target.addr), "port": get_name(link.target.addr)}
+        _source = {"block": get_instance_section(get_parent_instance_addr(link.source.addr)), "port": get_name(link.source.addr)}
+        _target = {"block": get_instance_section(get_parent_instance_addr(link.target.addr)), "port": get_name(link.target.addr)}
         link_list.append({"source": _source, "target": _target, "type": "interface"})
     return json.dumps({
         "blocks": block_list,
