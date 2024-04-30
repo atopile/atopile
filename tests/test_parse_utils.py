c38982b8a60c20e596917bd30c3233a82f71a024
Matthew Wildoer <mawildoer@gmail.com>
2024-04-29 19:33:31 -0700

    Add utils to reconstruct src better

---
+++ tests/test_parse_utils.py
@@ -0,0 +1,24 @@
+import pytest
+
+import atopile
+import atopile.parse
+import atopile.parse_utils
+
+
+def _parser(src: str):
+    input = atopile.parse.InputStream(src)
+    input.name = "test"
+    return atopile.parse.make_parser(input)
+
+
+@pytest.mark.parametrize(
+    "txt",
+    [
+        "a = 1",
+        "assert 1 within 2",
+        "b = 1kV +/- 10% + (2V - 1V)",
+    ]
+)
+def test_reconstructor_simple_stmt(txt: str):
+    """Ensure we can faithfully re-construct the source code from a parse tree"""
+    assert atopile.parse_utils.reconstruct(_parser(txt).simple_stmt()) == txt
