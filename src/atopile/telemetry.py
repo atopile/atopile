fe5088225fddbb53731eaa33c4a8c3226ce6a48a
Matthew Wildoer <mawildoer@gmail.com>
2024-07-01 22:23:39 -0700

    Ignore stderr on telemetry calls to remove red-herrings from log

--- src/atopile/telemetry.py
+++ src/atopile/telemetry.py
@@ -144,7 +144,7 @@ def get_current_git_hash() -> Optional[str]:
     """Get the current git commit hash."""
     try:
         return (
-            subprocess.check_output(["git", "rev-parse", "HEAD"])
+            subprocess.check_output(["git", "rev-parse", "HEAD"], stderr=subprocess.DEVNULL)
             .decode("ascii")
             .strip()
         )
