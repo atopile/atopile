56754899986ae68565170e90bbc2e0ff76ad2929
NoR8quoh1r <20768237+NoR8quoh1r@users.noreply.github.com>
2024-10-08 15:25:30 +0200

    Core: Add some arithmetic expressions

--- src/faebryk/core/parameter.py
+++ src/faebryk/core/parameter.py
@@ -2,18 +2,63 @@
 # SPDX-License-Identifier: MIT
 
 import logging
+import math
 from enum import Enum
 
 from faebryk.core.node import Node, f_field
 from faebryk.libs.sets import Range
-from faebryk.libs.units import Quantity, Unit
+from faebryk.libs.units import Quantity, Unit, P
+from typing import Protocol
 
 logger = logging.getLogger(__name__)
+
+from typing import runtime_checkable
+
+@runtime_checkable
+class HasUnit(Protocol):
+    unit: Unit
+
+#TODO: prohibit instantiation
+class Expression:
+    pass
+
+class Arithmetic(Expression):
+    def __init__(self, *operands):
+        types = [ int, float, Quantity, Parameter, Expression ]
+        if any(not type(op) in types for op in operands):
+            raise ValueError("operands must be int, float, Quantity, Parameter, or Expression")
+        self.operands = operands
+
+class Additive(Arithmetic):
+    def __init__(self, *operands):
+        super().__init__(*operands)
+        units = [ op.unit if isinstance(op, HasUnit) else P.dimensionless for op in operands ]
+        # Check if all units are compatible
+        self.unit = units[0]
+        if not all(u.is_compatible_with(self.unit) for u in units):
+            raise ValueError("All operands must have compatible units")
+
+class Add(Additive):
+    def __init__(self, *operands):
+        super().__init__(*operands)
+
+class Subtract(Additive):
+    def __init__(self, *operands):
+        super().__init__(*operands)
+
+class Multiply(Arithmetic):
+    def __init__(self, *operands):
+        super().__init__(*operands)
+        self.unit = math.prod([ op.unit if isinstance(op, HasUnit) else P.dimensionless for op in operands ])
+
+class Divide(Multiply):
+    def __init__(self, numerator, denominator):
+        super().__init__(numerator, denominator)
+        self.unit = numerator.unit / denominator.unit
 
 
 class Domain:
     pass
-
 
 class Domains:
     class ESeries(Domain):
@@ -33,10 +78,6 @@ class Domains:
             class Positive(Domain):
                 pass
 
-        class Reals(Domain):
-            class Positive(Domain):
-                pass
-
     class Boolean(Domain):
         pass
 
@@ -52,10 +93,10 @@ class Parameter(Node):
         *,
         unit: Unit | Quantity,
         # hard constraints
-        within: Range[Quantity] | None = None,
+        within: Range | None = None,
         domain: Domain = Domains.Numbers.Reals.Positive(),
         # soft constraints
-        soft_set: Range[Quantity] | None = None,
+        soft_set: Range| None = None,
         guess: Quantity | None = None,
         tolerance_guess: Quantity | None = None,
         # hints
@@ -67,6 +108,8 @@ class Parameter(Node):
         if not within.is_compatible_with_unit(unit):
             raise ValueError("incompatible units")
 
+        if not isinstance(unit, Unit):
+            raise TypeError("unit must be a Unit")
         self.unit = unit
         self.within = within
         self.domain = domain
