8865114053dc328a201381cff46971c0627bde89
Matthew Wildoer <mawildoer@gmail.com>
2024-10-24 15:13:22 -0700

    Test marking on wires as well

--- test/exporters/schematic/kicad/test_marking.py
+++ test/exporters/schematic/kicad/test_marking.py
@@ -15,16 +15,32 @@ def symbol():
     )
 
 
-def test_marking_in_program(symbol):
-    old_contents = Transformer._get_hash_contents(symbol)
+@pytest.fixture
+def wire():
+    return fileformats_sch.C_kicad_sch_file.C_kicad_sch.C_wire(
+        pts=fileformats_sch.C_pts(
+            xys=[
+                fileformats_sch.C_xy(0, 0),
+                fileformats_sch.C_xy(1, 1),
+            ]
+        ),
+        stroke=fileformats_sch.C_stroke(),
+    )
 
-    assert not Transformer.check_mark(symbol)
-    Transformer.mark(symbol)
 
-    new_contents = Transformer._get_hash_contents(symbol)
+@pytest.mark.parametrize("fixture_name", ["symbol", "wire"])
+def test_marking_in_program(fixture_name, request):
+    obj = request.getfixturevalue(fixture_name)
+
+    old_contents = Transformer._get_hash_contents(obj)
+
+    assert not Transformer.check_mark(obj)
+    Transformer.mark(obj)
+
+    new_contents = Transformer._get_hash_contents(obj)
 
     assert new_contents == old_contents
-    assert Transformer.check_mark(symbol)
+    assert Transformer.check_mark(obj)
 
 
 def test_marking_in_file(symbol, tmp_path):
