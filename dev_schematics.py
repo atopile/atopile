c2a2144fb0a550a766701f0ab476f8963673b2b7
Matthew Wildoer <mawildoer@gmail.com>
2024-10-31 16:51:55 -0700

    Skip picking

--- dev_schematics.py
+++ dev_schematics.py
@@ -30,7 +30,6 @@ assert isinstance(app.battery, F.Battery)
 assert isinstance(app.led, F.PoweredLED)
 
 
-# app.battery.add(F.has_descriptive_properties_defined({"LCSC": "C5239862"}))
 app.led.led.add(F.has_descriptive_properties_defined({"LCSC": "C7429912"}))
 app.led.current_limiting_resistor.add(F.has_descriptive_properties_defined({"LCSC": "C25077"}))
 
@@ -40,25 +39,25 @@ apply_design_to_pcb(app)
 full_transformer = Transformer(sch_file.kicad_sch, app.get_graph(), app)
 full_transformer.index_symbol_files(fp_lib_path_path, load_globals=False)
 
-# mimicing typically design/user-space
-audio_jack = full_transformer.app.add(Module())
-pin_s = audio_jack.add(F.Electrical())
-pin_t = audio_jack.add(F.Electrical())
-pin_r = audio_jack.add(F.Electrical())
-audio_jack.add(F.has_overriden_name_defined("U1"))
+# # mimicing typically design/user-space
+# audio_jack = full_transformer.app.add(Module())
+# pin_s = audio_jack.add(F.Electrical())
+# pin_t = audio_jack.add(F.Electrical())
+# pin_r = audio_jack.add(F.Electrical())
+# audio_jack.add(F.has_overriden_name_defined("U1"))
 
-# mimicing typically lcsc code
-sym = F.Symbol.with_component(
-    audio_jack,
-    {
-        "S": pin_s,
-        "T": pin_t,
-        "R": pin_r,
-    },
-)
-audio_jack.add(F.Symbol.has_symbol(sym))
-sym.add(F.Symbol.has_kicad_symbol("test:AudioJack-CUI-SJ-3523-SMT"))
+# # mimicing typically lcsc code
+# sym = F.Symbol.with_component(
+#     audio_jack,
+#     {
+#         "S": pin_s,
+#         "T": pin_t,
+#         "R": pin_r,
+#     },
+# )
+# audio_jack.add(F.Symbol.has_symbol(sym))
+# sym.add(F.Symbol.has_kicad_symbol("test:AudioJack-CUI-SJ-3523-SMT"))
 
-full_transformer.insert_symbol(audio_jack)
+# full_transformer.insert_symbol(audio_jack)
 
 full_transformer.generate_schematic()
