f6497b4b1d30574d272e13bab423b81519554d31
Matthew Wildoer <mawildoer@gmail.com>
2024-11-14 16:55:56 -0800

    Rebuild parser

--- src/atopile/parser/AtopileParser.py
+++ src/atopile/parser/AtopileParser.py
@@ -1,4 +1,4 @@
-# Generated from AtopileParser.g4 by ANTLR 4.13.1
+# Generated from AtopileParser.g4 by ANTLR 4.13.2
 # encoding: utf-8
 from antlr4 import *
 from io import StringIO
@@ -355,7 +355,7 @@ class AtopileParser ( AtopileParserBase ):
 
     def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
         super().__init__(input, output)
-        self.checkVersion("4.13.1")
+        self.checkVersion("4.13.2")
         self._interp = ParserATNSimulator(self, self.atn, self.decisionsToDFA, self.sharedContextCache)
         self._predicates = None
 
