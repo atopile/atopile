28db2f2caeeece57e2b3f5a8da84e131c934a1ac
Sam Mellor <sam@atopile.io>
2024-11-20 12:29:05 -0800

    build

--- src/atopile/cli/build.py
+++ src/atopile/cli/build.py
@@ -4,36 +4,48 @@ import itertools
 import json
 import logging
 import shutil
-from typing import Callable, Optional
+from typing import Annotated, Callable, Optional
 
-import click
+import typer
 
-import atopile.assertions
-import atopile.bom
 import atopile.config
-import atopile.front_end
-import atopile.layout
-import atopile.manufacturing_data
-import atopile.netlist
-import atopile.variable_report
-from atopile.cli.common import project_options
-from atopile.components import download_footprint
+
+# import atopile.assertions
+# import atopile.bom
+from atopile import errors
+
+# import atopile.front_end
+# import atopile.layout
+# import atopile.manufacturing_data
+# import atopile.netlist
+# import atopile.variable_report
+from atopile.cli.common import create_build_contexts
+
+# from atopile.components import download_footprint
 from atopile.config import BuildContext
 from atopile.errors import ExceptionAccumulator
-from atopile.instance_methods import all_descendants, match_components
-from atopile.netlist import get_netlist_as_str
+
+# from atopile.instance_methods import all_descendants, match_components
+# from atopile.netlist import get_netlist_as_str
 
 log = logging.getLogger(__name__)
 
 
-@click.command()
-@project_options
-def build(build_ctxs: list[BuildContext]):
+@errors.muffle_fatalities()
+@errors.log_ato_errors()
+def build(
+    entry: Annotated[str | None, typer.Argument()] = None,
+    build: Annotated[list[str], typer.Option(envvar="ATO_BUILD")] = [],
+    target: Annotated[list[str], typer.Option(envvar="ATO_TARGET")] = [],
+    option: Annotated[list[str], typer.Option(envvar="ATO_OPTION")] = [],
+):
     """
     Build the specified --target(s) or the targets specified by the build config.
-    Specify the root source file with the argument SOURCE.
+    Optionally specify a different entrypoint with the argument ENTRY.
     eg. `ato build --target my_target path/to/source.ato:module.path`
     """
+    build_ctxs = create_build_contexts(entry, build, target, option)
+
     with ExceptionAccumulator() as accumulator:
         for build_ctx in build_ctxs:
             log.info("Building %s", build_ctx.name)
@@ -68,10 +80,11 @@ def do_prebuild(build_ctx: BuildContext) -> None:
     with ExceptionAccumulator() as accumulator:
         # Solve the unknown variables
         if not build_ctx.dont_solve_equations:
-            with accumulator.collect():
-                atopile.assertions.simplify_expressions(build_ctx.entry)
-                atopile.assertions.solve_assertions(build_ctx)
-                atopile.assertions.simplify_expressions(build_ctx.entry)
+            # with accumulator.collect():
+            #     atopile.assertions.simplify_expressions(build_ctx.entry)
+            #     atopile.assertions.solve_assertions(build_ctx)
+            #     atopile.assertions.simplify_expressions(build_ctx.entry)
+            raise NotImplementedError("Equation solving is not implemented yet")
 
 
 def _do_build(build_ctx: BuildContext) -> None:
@@ -94,7 +107,7 @@ def _do_build(build_ctx: BuildContext) -> None:
         # Remove targets we don't know about, or are excluded
         excluded_targets = set(build_ctx.exclude_targets)
         known_targets = set(muster.targets.keys())
-        targets = set(targets) - excluded_targets & known_targets
+        targets = list(set(targets) - excluded_targets & known_targets)
 
         # Ensure the output directory exists
         build_ctx.output_base.parent.mkdir(parents=True, exist_ok=True)
@@ -119,8 +132,8 @@ class Muster:
     """A class to register targets to."""
 
     def __init__(self, logger: Optional[logging.Logger] = None) -> None:
-        self.targets = {}
-        self.do_by_default = []
+        self.targets: dict[str, TargetType] = {}
+        self.do_by_default: list[str] = []
         self.log = logger or logging.getLogger(__name__)
 
     def add_target(
@@ -187,61 +200,70 @@ def consolidate_3dmodels(build_args: BuildContext) -> None:
 @muster.register("netlist")
 def generate_netlist(build_args: BuildContext) -> None:
     """Generate a netlist for the project."""
-    with open(build_args.output_base.with_suffix(".net"), "w", encoding="utf-8") as f:
-        f.write(get_netlist_as_str(build_args.entry))
+    raise NotImplementedError("Netlist generation is not implemented yet")
+    # with open(build_args.output_base.with_suffix(".net"), "w", encoding="utf-8") as f:
+    #     f.write(get_netlist_as_str(build_args.entry))
 
 
 @muster.register("bom")
 def generate_bom(build_args: BuildContext) -> None:
     """Generate a BOM for the project."""
-    with open(build_args.output_base.with_suffix(".csv"), "w", encoding="utf-8") as f:
-        f.write(atopile.bom.generate_bom(build_args.entry))
+    raise NotImplementedError("BOM generation is not implemented yet")
+    # with open(build_args.output_base.with_suffix(".csv"), "w", encoding="utf-8") as f:
+    #     f.write(atopile.bom.generate_bom(build_args.entry))
 
 
 @muster.register("designator-map")
 def generate_designator_map(build_args: BuildContext) -> None:
     """Generate a designator map for the project."""
-    atopile.bom.generate_designator_map(build_args.entry)
+    raise NotImplementedError("Designator map generation is not implemented yet")
+    # atopile.bom.generate_designator_map(build_args.entry)
 
 
 @muster.register("mfg-data", default=False)
 def generate_manufacturing_data(build_ctx: BuildContext) -> None:
     """Generate a designator map for the project."""
-    atopile.manufacturing_data.generate_manufacturing_data(build_ctx)
+    raise NotImplementedError("Manufacturing data generation is not implemented yet")
+    # atopile.manufacturing_data.generate_manufacturing_data(build_ctx)
 
 
 @muster.register("drc", default=False)
 def generate_drc_report(build_ctx: BuildContext) -> None:
     """Generate a designator map for the project."""
-    atopile.manufacturing_data.generate_drc_report(build_ctx)
+    raise NotImplementedError("DRC report generation is not implemented yet")
+    # atopile.manufacturing_data.generate_drc_report(build_ctx)
 
 
 @muster.register("clone-footprints")
 def clone_footprints(build_args: BuildContext) -> None:
     """Clone the footprints for the project."""
-    all_components = filter(match_components, all_descendants(build_args.entry))
+    raise NotImplementedError("Footprint cloning is not implemented yet")
+    # all_components = filter(match_components, all_descendants(build_args.entry))
 
-    for component in all_components:
-        log.debug("Cloning footprint for %s", component)
-        download_footprint(
-            component,
-            footprint_dir=build_args.build_path / "footprints/footprints.pretty",
-        )
+    # for component in all_components:
+    #     log.debug("Cloning footprint for %s", component)
+    #     download_footprint(
+    #         component,
+    #         footprint_dir=build_args.build_path / "footprints/footprints.pretty",
+    #     )
 
 
 @muster.register("layout-module-map")
 def generate_module_map(build_args: BuildContext) -> None:
     """Generate a designator map for the project."""
-    atopile.layout.generate_module_map(build_args)
+    raise NotImplementedError("Module map generation is not implemented yet")
+    # atopile.layout.generate_module_map(build_args)
 
 
 @muster.register("assertions-report")
 def generate_assertion_report(build_ctx: BuildContext) -> None:
     """Generate a report based on assertions made in the source code."""
-    atopile.assertions.generate_assertion_report(build_ctx)
+    raise NotImplementedError("Assertion report generation is not implemented yet")
+    # atopile.assertions.generate_assertion_report(build_ctx)
 
 
 @muster.register("variable-report")
 def generate_variable_report(build_ctx: BuildContext) -> None:
     """Generate a report of all the variable values in the design."""
-    atopile.variable_report.generate(build_ctx)
+    raise NotImplementedError("Variable report generation is not implemented yet")
+    # atopile.variable_report.generate(build_ctx)
--- src/atopile/cli/common.py
+++ src/atopile/cli/common.py
@@ -2,13 +2,10 @@
 Common CLI writing utilities.
 """
 
-import functools
 import itertools
 import logging
 from pathlib import Path
 from typing import Iterable
-
-import click
 
 import atopile.config
 from atopile import address, errors, version
@@ -17,130 +14,75 @@ from atopile.address import AddrStr
 log = logging.getLogger(__name__)
 
 
-def project_options(f):
-    """
-    Utility decorator to ingest common config options to build a project.
-    """
+def get_entry_arg_file_path(entry: str | None) -> tuple[AddrStr | None, Path]:
+    # basic the entry address if provided, otherwise leave it as None
 
-    @click.argument("entry", required=False, default=None)
-    @click.option("-b", "--build", multiple=True, envvar="ATO_BUILD")
-    @click.option("-t", "--target", multiple=True, envvar="ATO_TARGET")
-    @click.option("-o", "--option", multiple=True, envvar="ATO_OPTION")
-    @functools.wraps(f)
-    @errors.muffle_fatalities()
-    @errors.log_ato_errors()
-    def wrapper(
-        *args,
-        entry: str,
-        build: Iterable[str],
-        target: Iterable[str],
-        option: Iterable[str],
-        **kwargs,
-    ):
-        """Wrap a CLI command to ingest common config options to build a project."""
+    if entry is None:
+        entry_arg_file_path = Path.cwd()
+    else:
+        entry = AddrStr(entry)
 
-        # basic the entry address if provided, otherwise leave it as None
-        if entry is not None:
-            entry = AddrStr(entry)
-
-            if address.get_file(entry) is None:
-                raise errors.AtoBadParameterError(
-                    f"Invalid entry address {entry} - entry must specify a file.",
-                    title="Bad 'entry' parameter",
-                )
-
-        # get the project
-        if entry is None:
-            entry_arg_file_path = Path.cwd()
-        else:
-            entry_arg_file_path = (
-                Path(address.get_file(entry)).expanduser().resolve().absolute()
-            )
-
-        try:
-            project_config = atopile.config.get_project_config_from_addr(
-                str(entry_arg_file_path)
-            )
-        except FileNotFoundError as ex:
-            # FIXME: this raises an exception when the entry is not in a project
+        if address.get_file(entry) is None:
             raise errors.AtoBadParameterError(
-                f"Could not find project from path {str(entry_arg_file_path)}. "
-                "Is this file path within a project?"
-            ) from ex
-
-        # Make sure I an all my sub-configs have appropriate versions
-        check_compiler_versions(project_config)
-
-        log.info("Using project %s", project_config.location)
-
-        # add custom config overrides
-        if option:
-            raise NotImplementedError(
-                "Custom config overrides have been removed in a refactor. "
-                "It's planned to re-add them in a future release. "
-                "If this is a blocker for you, please raise an issue. "
-                "In the meantime, you can use the `ato.yaml` file to set these options."
+                f"Invalid entry address {entry} - entry must specify a file.",
+                title="Bad 'entry' parameter",
             )
-        else:
-            config: atopile.config.ProjectConfig = project_config
 
-        # if we set an entry-point, we now need to deal with that
-        entry_addr_override = None
-        if entry:
-            if entry_arg_file_path.is_file():
-                if entry_section := address.get_entry_section(entry):
-                    entry_addr_override = address.from_parts(
-                        str(entry_arg_file_path.absolute()),
-                        entry_section,
-                    )
-                else:
-                    raise errors.AtoBadParameterError(
-                        "If an entry of a file is specified, you must specify"
-                        " the node within it you want to build.",
-                        title="Bad 'entry' parameter",
-                    )
+        entry_arg_file_path = (
+            Path(address.get_file(entry)).expanduser().resolve().absolute()
+        )
 
-            elif entry_arg_file_path.is_dir():
-                pass
+    return entry, entry_arg_file_path
 
-            elif not entry_arg_file_path.exists():
+
+def get_project_config(entry_arg_file_path: Path) -> atopile.config.ProjectConfig:
+    try:
+        project_config = atopile.config.get_project_config_from_addr(
+            str(entry_arg_file_path)
+        )
+    except FileNotFoundError as ex:
+        # FIXME: this raises an exception when the entry is not in a project
+        raise errors.AtoBadParameterError(
+            f"Could not find project from path {str(entry_arg_file_path)}. "
+            "Is this file path within a project?"
+        ) from ex
+
+    return project_config
+
+
+def check_entry_arg_file_path(
+    entry: AddrStr | None, entry_arg_file_path: Path
+) -> AddrStr | None:
+    entry_addr_override = None
+
+    if entry:
+        if entry_arg_file_path.is_file():
+            if entry_section := address.get_entry_section(entry):
+                entry_addr_override = address.from_parts(
+                    str(entry_arg_file_path.absolute()),
+                    entry_section,
+                )
+            else:
                 raise errors.AtoBadParameterError(
-                    "The entry you have specified does not exist.",
+                    "If an entry of a file is specified, you must specify"
+                    " the node within it you want to build.",
                     title="Bad 'entry' parameter",
                 )
-            else:
-                raise ValueError(
-                    f"Unexpected entry path type {entry_arg_file_path} - this should never happen!"
-                )
 
-        # Configure project context
-        project_ctx = atopile.config.ProjectContext.from_config(config)
-        atopile.config.set_project_context(project_ctx)
+        elif entry_arg_file_path.is_dir():
+            pass
 
-        # Make build contexts
-        with errors.handle_ato_errors(), errors.log_ato_errors():
-            if build_names := build or config.builds.keys():
-                build_ctxs: list[atopile.config.BuildContext] = [
-                    atopile.config.BuildContext.from_config_name(config, build_name)
-                    for build_name in build_names
-                ]
-            else:
-                build_ctxs = [
-                    atopile.config.BuildContext.from_config(
-                        "default", atopile.config.ProjectBuildConfig(), project_ctx
-                    )
-                ]
+        elif not entry_arg_file_path.exists():
+            raise errors.AtoBadParameterError(
+                "The entry you have specified does not exist.",
+                title="Bad 'entry' parameter",
+            )
+        else:
+            raise ValueError(
+                f"Unexpected entry path type {entry_arg_file_path} - this should never happen!"
+            )
 
-        for build_ctx in build_ctxs:
-            if entry_addr_override is not None:
-                build_ctx.entry = entry_addr_override
-            if target:
-                build_ctx.targets = list(target)
-
-        # Do the thing
-        return f(*args, **kwargs, build_ctxs=build_ctxs)
-
-    return wrapper
+    return entry_addr_override
 
 
 def check_compiler_versions(config: atopile.config.ProjectConfig):
@@ -153,7 +95,7 @@ def check_compiler_versions(config: atopile.config.ProjectConfig):
             errors.downgrade(
                 atopile.config.get_project_config_from_path, FileNotFoundError
             )(p)
-            for p in Path(config.location).glob("*")
+            for p in Path(config.location or ".").glob("*")
         )
 
         for cltr, cfg in errors.iter_through_errors(
@@ -177,3 +119,58 @@ def check_compiler_versions(config: atopile.config.ProjectConfig):
                         " built with this version of atopile "
                         f"({version.get_installed_atopile_version()})."
                     )
+
+
+def create_build_contexts(
+    entry: str | None,
+    build: Iterable[str],
+    target: Iterable[str],
+    option: Iterable[str],
+) -> list[atopile.config.BuildContext]:
+    entry, entry_arg_file_path = get_entry_arg_file_path(entry)
+    project_config = get_project_config(entry_arg_file_path)
+
+    # Make sure I an all my sub-configs have appropriate versions
+    check_compiler_versions(project_config)
+
+    log.info("Using project %s", project_config.location)
+
+    # add custom config overrides
+    if option:
+        raise NotImplementedError(
+            "Custom config overrides have been removed in a refactor. "
+            "It's planned to re-add them in a future release. "
+            "If this is a blocker for you, please raise an issue. "
+            "In the meantime, you can use the `ato.yaml` file to set these options."
+        )
+    else:
+        config: atopile.config.ProjectConfig = project_config
+
+    # if we set an entry-point, we now need to deal with that
+    entry_addr_override = check_entry_arg_file_path(entry, entry_arg_file_path)
+
+    # Configure project context
+    project_ctx = atopile.config.ProjectContext.from_config(config)
+    atopile.config.set_project_context(project_ctx)
+
+    # Make build contexts
+    with errors.handle_ato_errors(), errors.log_ato_errors():
+        if build_names := build or config.builds.keys():
+            build_ctxs: list[atopile.config.BuildContext] = [
+                atopile.config.BuildContext.from_config_name(config, build_name)
+                for build_name in build_names
+            ]
+        else:
+            build_ctxs = [
+                atopile.config.BuildContext.from_config(
+                    "default", atopile.config.ProjectBuildConfig(), project_ctx
+                )
+            ]
+
+    for build_ctx in build_ctxs:
+        if entry_addr_override is not None:
+            build_ctx.entry = entry_addr_override
+        if target:
+            build_ctx.targets = list(target)
+
+    return build_ctxs
