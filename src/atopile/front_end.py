ead1654e00a12258dbef5dec9f6eb1e2aad601d9
Matt Wildoer <matt@atopile.io>
2024-11-29 14:23:34 -0800

    review improvements

--- src/atopile/front_end.py
+++ src/atopile/front_end.py
@@ -351,7 +351,9 @@ class Wendy(BasicsMixin, SequenceMixin, AtopileParserVisitor):
                         )
 
                     name = ref[0]
-                    if not hasattr(F, name):
+                    if not hasattr(F, name) or not isinstance(
+                        getattr(F, name), (L.Module, L.ModuleInterface)
+                    ):
                         raise errors.UserKeyError.from_ctx(
                             ctx, f'Unknown standard library module: "{name}"'
                         )
