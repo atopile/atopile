e3b96b15bd2a48724ea81b798233a08c73d69795
Matt Wildoer <matt@atopile.io>
2024-12-03 16:55:55 -0800

    Fix address typing

--- src/atopile/address.py
+++ src/atopile/address.py
@@ -6,6 +6,7 @@ Addresses go by other names in various files for historical reasons - but should
 This file provides utilities for working with addresses.
 """
 
+import os
 from functools import wraps
 from os import PathLike
 from pathlib import Path
@@ -29,7 +30,10 @@ class AddrStr(str):
 
     @classmethod
     def from_parts(
-        cls, file: str, entry: Optional[str] = None, instance: Optional[str] = None
+        cls,
+        file: str | os.PathLike | None,
+        entry: str | None = None,
+        instance: str | None = None,
     ) -> "AddrStr":
         return cls(from_parts(file, entry, instance))
 
@@ -143,10 +147,10 @@ def add_instance(address: AddrStr, instance: str) -> AddrStr:
     current_instance_addr = get_instance_section(address)
     if current_instance_addr is not None:
         if current_instance_addr == "":
-            return address + instance
-        return address + "." + instance
+            return AddrStr(address + instance)
+        return AddrStr(address + "." + instance)
     elif get_entry_section(address):
-        return address + "::" + instance
+        return AddrStr(address + "::" + instance)
     else:
         raise AddressError("Cannot add instance to something without an entry section.")
 
@@ -171,9 +175,9 @@ def add_entry(address: AddrStr, entry: str) -> AddrStr:
         raise AddressError("Cannot add entry to an instance address.")
 
     if not get_entry_section(address):
-        return address + ":" + entry
+        return AddrStr(address + ":" + entry)
     else:
-        return address + "." + entry
+        return AddrStr(address + "." + entry)
 
 
 def add_entries(address: AddrStr, entries: Iterable[str]) -> AddrStr:
@@ -187,12 +191,14 @@ def add_entries(address: AddrStr, entries: Iterable[str]) -> AddrStr:
 
 
 def from_parts(
-    file: str | None, entry: str | None = None, instance: str | None = None
+    file: str | None | os.PathLike,
+    entry: str | None = None,
+    instance: str | None = None,
 ) -> AddrStr:
     """
     Create an address from its parts.
     """
-    address = str(file) if file else ""
+    address = AddrStr(file) if file else AddrStr("")
     if entry:
         address = add_entry(address, entry)
     if instance:
@@ -209,9 +215,9 @@ def get_parent_instance_addr(address: AddrStr) -> Optional[AddrStr]:
         return None
 
     if "." in instance_section:
-        return address.rsplit(".", 1)[0]
+        return AddrStr(address.rsplit(".", 1)[0])
 
-    return address.rsplit("::", 1)[0]
+    return AddrStr(address.rsplit("::", 1)[0])
 
 
 def get_instance_names(address: AddrStr) -> list[str]:
