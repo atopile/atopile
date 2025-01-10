91b4a8e965aecc054f2b9e372b0b0b2db2368e96
Narayan Powderly <narayan@atopile.io>
2025-01-09 16:05:16 -0800

    change footprint to atopile address, make message clearer

--- src/faebryk/exporters/pcb/kicad/transformer.py
+++ src/faebryk/exporters/pcb/kicad/transformer.py
@@ -281,8 +281,8 @@ class PCB_Transformer:
             elif fp := fps_by_path.get(f"/{hashed_addr}/{hashed_addr}"):
                 with downgrade(DeprecatedException):
                     raise DeprecatedException(
-                        f"{fp.name} is linked using v0.2 mechanism, "
-                        "please save the design to update."
+                        f"`{node.get_full_name()}` is linked to the layout using v0.2"
+                        " mechanism, please save the design to update."
                     )
                 footprint_map[node] = fp
 
