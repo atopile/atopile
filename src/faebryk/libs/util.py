c7b0a4a91d40d9750e9d65c09c8b3b2db5a446c1
Matt Wildoer <matt@atopile.io>
2025-01-12 17:00:34 -0800

    Fix type complaints in FuncSet

--- src/faebryk/libs/util.py
+++ src/faebryk/libs/util.py
@@ -1102,19 +1102,25 @@ class FuncSet[T, H: Hashable = int](collections.abc.MutableSet[T]):
         for item in data:
             self._deref[self._hasher(item)].append(item)
 
-    def add(self, item: T):
-        if item not in self._deref[self._hasher(item)]:
-            self._deref[self._hasher(item)].append(item)
+    def add(self, value: T):
+        if value not in self._deref[self._hasher(value)]:
+            self._deref[self._hasher(value)].append(value)
 
-    def discard(self, item: T):
-        hashed = self._hasher(item)
-        if hashed in self._deref and item in self._deref[hashed]:
-            self._deref[hashed].remove(item)
+    def discard(self, value: T):
+        hashed = self._hasher(value)
+        if hashed in self._deref and value in self._deref[hashed]:
+            self._deref[hashed].remove(value)
             if not self._deref[hashed]:
                 del self._deref[hashed]
 
-    def __contains__(self, item: T):
-        return item in self._deref[self._hasher(item)]
+    def __contains__(self, value: object):
+        try:
+            # Allow something of broader type than typically allowed
+            # but ultimately behave the same
+            hash_value = self._hasher(value)  # type: ignore
+        except Exception:
+            return False
+        return value in self._deref[hash_value]
 
     def __iter__(self) -> Iterator[T]:
         yield from chain.from_iterable(self._deref.values())
