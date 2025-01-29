9d2a39fa3c6ebf00af83fd47503aa21e4a062d79
Sam Mellor <sam@atopile.io>
2025-01-28 23:30:37 -0800

    add damping ratio to LC filter

--- src/faebryk/library/FilterElectricalLC.py
+++ src/faebryk/library/FilterElectricalLC.py
@@ -23,6 +23,7 @@ class FilterElectricalLC(F.Filter):
     out: F.ElectricSignal
     capacitor: F.Capacitor
     inductor: F.Inductor
+    damping_ratio = L.p_field(domain=L.Domains.Numbers.REAL())
 
     z0 = L.p_field(units=P.ohm)
 
@@ -43,13 +44,24 @@ class FilterElectricalLC(F.Filter):
     def build_lowpass(self):
         Li = self.inductor.inductance
         C = self.capacitor.capacitance
+        R = self.inductor.dc_resistance
         fc = self.cutoff_frequency
+        damping_ratio = self.damping_ratio
 
         # TODO other orders & types
         self.order.constrain_subset(2)
         self.response.constrain_subset(F.Filter.Response.LOWPASS)
 
+        # cutoff frequency
         fc.alias_is(1 / (2 * math.pi * (C * Li).operation_sqrt()))
+        C.alias_is(1 / (4 * math.pi**2 * fc**2 * Li))
+        Li.alias_is(1 / (4 * math.pi**2 * fc**2 * C))
+
+        # damping ratio
+        damping_ratio.alias_is(R / 2 * (C / Li).operation_sqrt())
+        R.alias_is(2 * damping_ratio / (C / Li).operation_sqrt())
+        C.alias_is(4 * damping_ratio**2 * Li / R**2)
+        Li.alias_is(C * R**2 / (4 * damping_ratio**2))
 
         # low pass
         self.in_.line.connect_via(
