836a6a6cc720b149ba59501b33489a201f258c97
Matt Wildoer <matt@atopile.io>
2025-02-06 15:57:06 -0800

    Add testing for PickErrors

--- test/libs/picker/test_pickers.py
+++ test/libs/picker/test_pickers.py
@@ -6,6 +6,7 @@ import sys
 from pathlib import Path
 from tempfile import mkdtemp
 from typing import TYPE_CHECKING
+from unittest.mock import MagicMock
 
 import pytest
 
@@ -15,7 +16,11 @@ from faebryk.core.module import Module
 from faebryk.core.solver.defaultsolver import DefaultSolver
 from faebryk.libs.library import L
 from faebryk.libs.picker.api.picker_lib import get_candidates, pick_atomically
-from faebryk.libs.picker.picker import pick_part_recursively
+from faebryk.libs.picker.picker import (
+    PickError,
+    pick_part_recursively,
+    pick_topologically,
+)
 from faebryk.libs.sets.sets import EnumSet
 from faebryk.libs.units import P
 from faebryk.libs.util import groupby
@@ -183,3 +188,34 @@ def test_reject_diode_for_led():
     ok = pick_atomically([(led, c) for c in candidates[diode]], solver)
 
     assert not ok
+
+
+def test_pick_error_group():
+    # a is a module with a LCSC ID that doesn't exist
+    class DoesntExist(L.Module):
+        lcsc_id = L.f_field(F.has_descriptive_properties_defined)(
+            {"LCSC": "C987654321"}
+        )
+
+    root = L.Module()
+
+    # a and b are modules with LCSC IDs that don't exist
+    a = DoesntExist()
+    b = DoesntExist()
+
+    # Good luck finding a 10 gigafarad capacitor!
+    c = F.Capacitor()
+    c.add(F.has_package(F.has_package.Package.R0402))
+    c.capacitance.alias_is(L.Range.from_center_rel(10 * P.GF, 0.1))
+
+    root.add(a)
+    root.add(b)
+    root.add(c)
+
+    solver = DefaultSolver()
+
+    with pytest.raises(ExceptionGroup) as ex:
+        pick_topologically(root.get_tree(types=L.Module), solver, MagicMock())
+
+    assert len(ex.value.exceptions) == 3
+    assert all(isinstance(e, PickError) for e in ex.value.exceptions)
