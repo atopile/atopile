a2420582c914402dbf890a3e36b219c2ec9653c4
ruben-iteng <94007802+ruben-iteng@users.noreply.github.com>
2025-02-23 22:33:26 +0100

    Add VoltageDivider
    Depricate ResistorVoltageDivider

--- src/faebryk/library/VoltageDivider.py
+++ src/faebryk/library/VoltageDivider.py
@@ -3,8 +3,6 @@
 
 import logging
 
-from deprecated import deprecated
-
 import faebryk.library._F as F
 from faebryk.core.module import Module
 from faebryk.libs.library import L
@@ -13,11 +11,11 @@ from faebryk.libs.units import P
 logger = logging.getLogger(__name__)
 
 
-@deprecated(reason="Use VoltageDivider instead")
-class ResistorVoltageDivider(Module):
+class VoltageDivider(Module):
     resistor = L.list_field(2, F.Resistor)
 
-    node = L.list_field(3, F.Electrical)
+    power_in: F.ElectricPower
+    power_out: F.ElectricPower
 
     total_resistance = L.p_field(units=P.Ω)
     ratio = L.p_field(units=P.dimensionless)
@@ -25,12 +23,19 @@ class ResistorVoltageDivider(Module):
 
     @L.rt_field
     def can_bridge(self):
-        return F.can_bridge_defined(self.node[0], self.node[1])
+        return F.can_bridge_defined(self.power_in, self.power_out)
 
     def __preinit__(self):
-        self.node[0].connect_via([self.resistor[0], self.resistor[1]], self.node[2])
-        self.node[0].connect_via([self.resistor[0]], self.node[1])
+        self.power_in.hv.connect_via(self.resistor[0], self.power_out.hv)
+        self.power_out.hv.connect_via(self.resistor[1], self.power_out.lv)
+        self.power_in.lv.connect(self.power_out.lv)
 
+        self.power_in.make_sink()
+        self.power_out.make_source()
+
+        # maximum sinkable current
+        self.max_current.alias_is(self.power_out.max_current)
+        self.ratio.alias_is(self.resistor[0].resistance / self.total_resistance)
         self.total_resistance.alias_is(
             self.resistor[0].resistance + self.resistor[1].resistance
         )
@@ -42,3 +47,4 @@ class ResistorVoltageDivider(Module):
         self.resistor[1].resistance.alias_is(
             self.total_resistance - self.resistor[0].resistance
         )
+        # TODO: calculate maximum power dissipation of resistors
