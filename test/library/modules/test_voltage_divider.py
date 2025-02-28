4048a9de38b0ab4f51bef3c5595f1c6b180ceb88
ruben-iteng <94007802+ruben-iteng@users.noreply.github.com>
2025-02-28 10:58:28 +0100

    Add test frame

---
+++ test/library/modules/test_voltage_divider.py
@@ -0,0 +1,12 @@
+# This file is part of the faebryk project
+# SPDX-License-Identifier: MIT
+
+
+def test_ratio():
+    """Test if ratio is correct"""
+    ...
+
+
+def test_resistance():
+    """Test if resistor resistances are correct"""
+    ...
