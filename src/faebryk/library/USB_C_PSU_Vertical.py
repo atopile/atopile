3f7cd53078bd06bdb79aae610d175da7fed75cdd
iopapamanoglou <ioannis@atopile.io>
2025-03-04 18:52:14 +0100

    attach fuse; fix example

--- src/faebryk/library/USB_C_PSU_Vertical.py
+++ src/faebryk/library/USB_C_PSU_Vertical.py
@@ -34,8 +34,8 @@ class USB_C_PSU_Vertical(Module):
         for res in self.configuration_resistors:
             res.resistance.constrain_subset(L.Range.from_center_rel(5.1 * P.kohm, 0.05))
 
-        vbus = self.usb_connector.vbus.fused()
-        vbus_fuse = vbus.get_first_child_of_type(F.Fuse)
+        vbus = self.usb_connector.vbus.fused(self)
+        vbus_fuse = self.get_first_child_of_type(F.Fuse)
         vbus_fuse.fuse_type.constrain_subset(F.Fuse.FuseType.RESETTABLE)
         vbus_fuse.trip_current.constrain_subset(L.Range.from_center_rel(1 * P.A, 0.05))
 
