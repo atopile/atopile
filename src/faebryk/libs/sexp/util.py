689facc6594493e4eff791d639d0beb354eead49
Sam Mellor <sam@atopile.io>
2025-03-18 12:12:46 -0700

    match kicad sexp formatting

--- src/faebryk/libs/sexp/util.py
+++ src/faebryk/libs/sexp/util.py
@@ -10,6 +10,7 @@ def prettify_sexp_string(raw: str) -> str:
     out = ""
     level = 0
     in_quotes = False
+    in_leaf_expr = True
     for c in raw:
         if c == '"':
             in_quotes = not in_quotes
@@ -20,11 +21,15 @@ def prettify_sexp_string(raw: str) -> str:
         elif c == " " and out[-1] == " ":
             continue
         elif c == "(":
+            in_leaf_expr = True
             if level != 0:
                 out += "\n" + " " * 4 * level
             level += 1
         elif c == ")":
             level -= 1
+            if not in_leaf_expr:
+                out += "\n" + " " * 4 * level
+            in_leaf_expr = False
         out += c
 
     # if i > 0 no strip is a kicad bug(?) workaround
