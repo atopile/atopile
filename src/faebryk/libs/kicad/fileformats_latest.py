976be6f33d781bef040b1cdc14fa325a0f747e06
Sam Mellor <sam@atopile.io>
2025-03-19 17:09:49 -0700

    C_polygon is a shape?

--- src/faebryk/libs/kicad/fileformats_latest.py
+++ src/faebryk/libs/kicad/fileformats_latest.py
@@ -608,7 +608,7 @@ class C_rect(C_shape):
 # gr_poly
 # fp_poly
 @dataclass(kw_only=True)
-class C_polygon(_SingleOrMultiLayer):
+class C_polygon(C_shape):
     pts: C_pts = field(**sexp_field(order=-1))
 
 
