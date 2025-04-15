37d50b175cc9c40b93765a1045d913471e24e9c9
Sam Mellor <sam@atopile.io>
2025-04-14 17:59:43 -0700

    predicated_once

--- src/faebryk/library/is_pulled.py
+++ src/faebryk/library/is_pulled.py
@@ -1,7 +1,7 @@
 import faebryk.library._F as F
 from faebryk.core.module import Module
 from faebryk.core.pathfinder import find_paths
-from faebryk.libs.util import once
+from faebryk.libs.util import predicated_once
 
 
 class is_pulled(Module.TraitT.decless()):
@@ -9,7 +9,7 @@ class is_pulled(Module.TraitT.decless()):
         super().__init__()
         self.signal = signal
 
-    @once  # TODO: only cache once True
+    @predicated_once(lambda result: result is True)
     def check(self) -> bool:
         # TODO: more efficient method?
         paths = find_paths(self.signal, [self.signal.reference])
