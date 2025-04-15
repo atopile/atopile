fafdc8cf3ae723a254bf303cf45ca23a8c9d69ff
Sam Mellor <sam@atopile.io>
2025-04-15 10:52:25 -0700

    test

---
+++ test/library/test_design_checks.py
@@ -0,0 +1,23 @@
+import pytest
+
+import faebryk.library._F as F
+from faebryk.core.module import Module
+from faebryk.library.requires_pulls import RequiresPullNotFulfilled
+from faebryk.libs.app.checks import check_design
+
+
+def test_i2c_requires_pulls():
+    class App(Module):
+        a: F.I2C
+        b: F.I2C
+
+        def __postinit__(self):
+            self.a.connect(self.b)
+
+    app = App()
+
+    with pytest.raises(RequiresPullNotFulfilled):
+        check_design(app.get_graph())
+
+    app.a.terminate(app)
+    check_design(app.get_graph())
