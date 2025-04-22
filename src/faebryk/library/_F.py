5773ff254566bb31a10f89709e03fdf34abd3386
iopapamanoglou <ioannis@atopile.io>
2025-04-22 23:41:14 +0000

    cycle

--- src/faebryk/library/_F.py
+++ src/faebryk/library/_F.py
@@ -29,7 +29,6 @@ from faebryk.library.is_bus_parameter import is_bus_parameter
 from faebryk.library.Signal import Signal
 from faebryk.library.is_optional_defined import is_optional_defined
 from faebryk.library.has_footprint import has_footprint
-from faebryk.library.has_solver import has_solver
 from faebryk.library.Mechanical import Mechanical
 from faebryk.library.has_overriden_name import has_overriden_name
 from faebryk.library.is_pickable import is_pickable
@@ -38,6 +37,7 @@ from faebryk.library.has_reference import has_reference
 from faebryk.library.has_descriptive_properties import has_descriptive_properties
 from faebryk.library.is_app_root import is_app_root
 from faebryk.library.has_layout_transform import has_layout_transform
+from faebryk.library.has_solver import has_solver
 from faebryk.library.can_bridge import can_bridge
 from faebryk.library.can_specialize import can_specialize
 from faebryk.library.has_datasheet import has_datasheet
@@ -68,13 +68,13 @@ from faebryk.library.has_single_electric_reference_defined import has_single_ele
 from faebryk.library.Filter import Filter
 from faebryk.library.Logic import Logic
 from faebryk.library.Footprint import Footprint
-from faebryk.library.implements_design_check import implements_design_check
 from faebryk.library.has_overriden_name_defined import has_overriden_name_defined
 from faebryk.library.is_pickable_by_part_number import is_pickable_by_part_number
 from faebryk.library.is_pickable_by_supplier_id import is_pickable_by_supplier_id
 from faebryk.library.is_pickable_by_type import is_pickable_by_type
 from faebryk.library.has_linked_pad_defined import has_linked_pad_defined
 from faebryk.library.Symbol import Symbol
+from faebryk.library.implements_design_check import implements_design_check
 from faebryk.library.can_bridge_defined import can_bridge_defined
 from faebryk.library.has_datasheet_defined import has_datasheet_defined
 from faebryk.library.has_part_removed import has_part_removed
@@ -90,11 +90,11 @@ from faebryk.library.can_attach_to_footprint import can_attach_to_footprint
 from faebryk.library.can_attach_via_pinmap import can_attach_via_pinmap
 from faebryk.library.has_footprint_impl import has_footprint_impl
 from faebryk.library.has_kicad_footprint import has_kicad_footprint
-from faebryk.library.PCB import PCB
-from faebryk.library.requires_external_usage import requires_external_usage
 from faebryk.library.has_descriptive_properties_defined import has_descriptive_properties_defined
 from faebryk.library.Pad import Pad
 from faebryk.library.has_symbol_layout import has_symbol_layout
+from faebryk.library.PCB import PCB
+from faebryk.library.requires_external_usage import requires_external_usage
 from faebryk.library.Button import Button
 from faebryk.library.GDT import GDT
 from faebryk.library.BJT import BJT
