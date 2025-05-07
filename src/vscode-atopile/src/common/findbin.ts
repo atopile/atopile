919742ead62748b012510303ddc805bb5e0af95e
iopapamanoglou <ioannis@atopile.io>
2025-05-07 10:06:07 -0700

    use system ato (if no py or setting)

--- src/vscode-atopile/src/common/findbin.ts
+++ src/vscode-atopile/src/common/findbin.ts
@@ -4,6 +4,7 @@ import { onDidChangeConfiguration } from './vscodeapi';
 import * as fs from 'fs';
 import { ISettings } from './settings';
 import { traceInfo, traceVerbose } from './log/logging';
+const which = require('which');
 
 export interface AtoBinInfo {
     init: boolean;
@@ -13,12 +14,23 @@ const onDidChangeAtoBinInfoEvent = new EventEmitter<AtoBinInfo>();
 export const onDidChangeAtoBinInfo: Event<AtoBinInfo> = onDidChangeAtoBinInfoEvent.event;
 
 let g_pyAtoBin: string | undefined = undefined;
+let g_sysAtoBin: string | undefined = undefined;
 
 export function getAtoBin(settings?: ISettings): string | undefined {
     if (settings && settings.ato) {
+        traceInfo(`Using ato bin from settings: ${settings.ato}`);
         return settings.ato;
     }
-    return g_pyAtoBin;
+    if (g_pyAtoBin) {
+        traceInfo(`Using ato bin from venv: ${g_pyAtoBin}`);
+        return g_pyAtoBin;
+    }
+    if (g_sysAtoBin) {
+        traceInfo(`Using ato bin from system PATH: ${g_sysAtoBin}`);
+        return g_sysAtoBin;
+    }
+    traceVerbose(`No ato bin found.`);
+    return undefined;
 }
 
 export async function initAtoBin(disposables: Disposable[]): Promise<void> {
@@ -44,4 +56,13 @@ export async function initAtoBin(disposables: Disposable[]): Promise<void> {
     );
 
     await initializePython(disposables);
+
+    // check if ato in system path
+    g_sysAtoBin = await which('ato', { nothrow: true });
+    if (g_sysAtoBin) {
+        traceInfo(`Ato bin found in system PATH: ${g_sysAtoBin}`);
+        onDidChangeAtoBinInfoEvent.fire({ init: true });
+    } else {
+        traceVerbose(`Ato bin not found in system PATH.`);
+    }
 }
