4cb7b80d812cb1b3fb1e26d3daf9beefe72273d7
Elen Eisendle <ln@calcifer.ee>
2025-06-16 19:05:44 +0200

    add test for the negative voltage divider

--- test/libs/picker/test_pickers.py
+++ test/libs/picker/test_pickers.py
@@ -254,6 +254,18 @@ def test_pick_dependency_advanced_2():
     solver = DefaultSolver()
     pick_part_recursively(rdiv, solver)
 
+@pytest.mark.usefixtures("setup_project_config")
+@pytest.mark.slow
+def test_pick_dependency_div_negative():
+    rdiv = F.ResistorVoltageDivider()
+
+    rdiv.v_in.alias_is(L.Range(-10 * P.V, -9 * P.V))
+    rdiv.v_out.constrain_subset(L.Range(-3.2 * P.V, -3 * P.V))
+    rdiv.max_current.constrain_subset(L.Range(1 * P.mA, 3 * P.mA))
+
+    solver = DefaultSolver()
+    pick_part_recursively(rdiv, solver)
+
 
 @pytest.mark.usefixtures("setup_project_config")
 def test_null_solver():
