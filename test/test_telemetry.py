95b66d13aa98aa502f984e62a59283c46b41f3fc
Sam Mellor <sam@atopile.io>
2025-06-27 11:37:47 -0700

    test client init / event capture

--- test/test_telemetry.py
+++ test/test_telemetry.py
@@ -14,3 +14,19 @@ def test_normalize_git_remote_url(git_remote):
     assert (
         telemetry._normalize_git_remote_url(git_remote) == "github.com/atopile/atopile"
     )
+
+
+def test_init_client():
+    assert isinstance(telemetry.client, telemetry.Posthog)
+
+
+def test_capture_event():
+    @telemetry.capture(
+        "test_start",
+        "test_end",
+        {"test_property": "test_value"},
+    )
+    def test_capture_event():
+        pass
+
+    test_capture_event()
