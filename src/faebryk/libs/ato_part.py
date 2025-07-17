749d626c313a1095ddfd07ebb871267d2f79d94a
Sam Mellor <sam@atopile.io>
2025-07-16 19:27:57 -0700

    filter in lifecycle manager

--- src/faebryk/libs/ato_part.py
+++ src/faebryk/libs/ato_part.py
@@ -103,21 +103,7 @@ class AtoPart:
             self.fp.footprint.models[0].path = rel_path
 
     def compare(self, other: Self) -> dict:
-        def _filter_datasheet_downgrades(diff: dict) -> dict:
-            """
-            EasyEDA ingestion only sometimes returns a datasheet. In the case where we
-            did have one previously, filter that update out of the diff so we don't
-            then remove it from the part definition.
-            """
-
-            if (datasheet := diff.get(".datasheet")) is not None:
-                before, after = datasheet
-                if before is not None and after is None:
-                    del diff[".datasheet"]
-
-            return diff
-
-        diff = compare_dataclasses(
+        return compare_dataclasses(
             self,
             other,
             skip_keys=(
@@ -127,10 +113,6 @@ class AtoPart:
                 "checksum",
             ),
         )
-
-        diff = _filter_datasheet_downgrades(diff)
-
-        return diff
 
     def _dump_pins(self, build: AtoCodeGen.ComponentFile):
         symbol = first(self.symbol.kicad_symbol_lib.symbols.values())
