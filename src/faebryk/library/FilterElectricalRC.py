b17332f3b23e938366faf1a72afc7756e8cb7db2
Nicholas Krstevski <nick@atopile.io>
2025-07-29 18:43:35 -0700

    test effectiveness of fix

--- src/faebryk/library/FilterElectricalRC.py
+++ src/faebryk/library/FilterElectricalRC.py
@@ -49,3 +49,37 @@ class FilterElectricalRC(F.Filter):
 
         # Set the max voltage of the capacitor to min 1.5 times the output voltage
         self.capacitor.max_voltage.constrain_ge(self.out.reference.voltage * 1.5)
+
+    @L.rt_field
+    def single_electric_reference(self):
+        return F.has_single_electric_reference_defined(
+            F.ElectricLogic.connect_all_module_references(self)
+        )
+
+    @L.rt_field
+    def can_bridge(self):
+        return F.can_bridge_defined(self.in_.line, self.out.line)
+
+    @classmethod
+    def hardcoded_fc(cls, fc: Quantity_Set, r: Quantity_Set, c: Quantity_Set):
+        #TODO: Remove hardcoded when normal equations solve faster
+        self = cls(_hardcoded=True)
+        r = None
+        c = None
+        self.cutoff_frequency.constrain_subset(fc)
+        self.resistor.resistance.constrain_subset(r)
+        self.capacitor.capacitance.constrain_subset(c)
+        return self
+
+    @classmethod
+    def hardcoded_rc(cls, resistance: Quantity_Set, capacitance: Quantity_Set):
+        self = cls(_hardcoded=True)
+        fc = 1 / (
+            Quantity_Interval_Disjoint.from_value(resistance * capacitance)
+            * 2
+            * math.pi
+        )
+        self.resistor.resistance.constrain_subset(resistance)
+        self.capacitor.capacitance.constrain_subset(capacitance)
+        self.cutoff_frequency.constrain_subset(fc)
+        return self
