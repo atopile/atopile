e5ce4d189eff05c2c508cd81824a48c9a55ec4f2
Nicholas Krstevski <nick@atopile.io>
2025-08-07 14:30:02 -0700

    externally invisible reorganize into subfolders

--- src/faebryk/library/Fan.py
+++
@@ -1,9 +0,0 @@
-# This file is part of the faebryk project
-# SPDX-License-Identifier: MIT
-
-import faebryk.library._F as F
-from faebryk.core.module import Module
-
-
-class Fan(Module):
-    power: F.ElectricPower
--- src/faebryk/library/NFET.py
+++
@@ -1,9 +0,0 @@
-# This file is part of the faebryk project
-# SPDX-License-Identifier: MIT
-
-import faebryk.library._F as F
-
-
-class NFET(F.MOSFET):
-    def __preinit__(self) -> None:
-        self.channel_type.alias_is(F.MOSFET.ChannelType.N_CHANNEL)
