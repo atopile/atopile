542c6337b1e1b7c0b0e226d545a9e3604c31863f
Sam Mellor <sam@atopile.io>
2025-08-25 15:24:48 -0700

    default to system cert stores

--- src/faebryk/libs/http.py
+++ src/faebryk/libs/http.py
@@ -1,7 +1,9 @@
 import contextlib
+import ssl
 from collections.abc import Generator
 
 import httpx
+import truststore
 from httpx import (  # noqa: F401
     HTTPError,
     HTTPStatusError,
@@ -13,8 +15,11 @@ from httpx import (  # noqa: F401
 
 @contextlib.contextmanager
 def http_client(
-    headers: dict[str, str] | None = None, verify: bool = True
+    headers: dict[str, str] | None = None, verify: bool | ssl.SSLContext = True
 ) -> Generator[httpx.Client, None, None]:
+    if verify is True:
+        verify = truststore.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+
     client = httpx.Client(headers=headers, verify=verify)
 
     yield client
