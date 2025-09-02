7e38327cd213ba7aad418ec27b019d172268df1d
Nicholas Krstevski <nick@atopile.io>
2025-09-02 14:49:27 -0700

    cleaning up pytest and script

--- scripts/build_examples.py
+++ scripts/build_examples.py
@@ -1,4 +1,5 @@
 import ast
+import importlib
 import textwrap
 from pathlib import Path
 from textwrap import dedent
@@ -7,6 +8,9 @@ import faebryk.library._F as F
 from atopile.datatypes import TypeRef
 from atopile.front_end import Bob
 from atopile.parse import parse_text_as_file
+from faebryk.core.module import Module
+from faebryk.core.moduleinterface import ModuleInterface
+from faebryk.core.trait import Trait
 from faebryk.libs.library import L
 
 
@@ -38,24 +42,35 @@ def extract_usage_example_runtime(module_name: str) -> tuple[str, str]:
 
     return textwrap.dedent(example).strip("\n"), str(language)
 
-def extract_usage_example_ast(file_path: str) -> tuple[str, str]:
+def extract_usage_example_ast(file_path: str) -> tuple[str | None, str | None]:
     """
     Parse the usage example trait implementation and return (example, language).
     """
-    LIBRARY_PATH = Path(F.__file__).parent
-    file_path = LIBRARY_PATH / f"{file_path}"
+    example = None
+    language = None
 
-    content = file_path.read_text()
+    LIBRARY_PATH = Path(F.__file__).parent
+    abs_file_path = LIBRARY_PATH / f"{file_path}"
+
+    content = abs_file_path.read_text()
     tree = ast.parse(content)
     for node in ast.walk(tree):
-        if isinstance(node, ast.Assign):
-            for target in node.targets:
-                if isinstance(target, ast.Name):
-                    if target.id == "usage_example":
-                        example = node.value.keywords[0].value.value
-                        language = node.value.keywords[1].value.attr
-                        return example, language
-    return "None", "None"
+        if not isinstance(node, ast.Assign):
+            continue
+        if not any(isinstance(t, ast.Name) and t.id == "usage_example" for t in node.targets):
+            continue
+        if not isinstance(node.value, ast.Call):
+            continue
+        if not isinstance(node.value.keywords, list):
+            continue
+        if len(node.value.keywords) != 2:
+            continue
+        if isinstance(node.value.keywords[0].value, ast.Constant):
+            example = str(node.value.keywords[0].value.value)
+        if isinstance(node.value.keywords[1].value, ast.Attribute):
+            language = str(node.value.keywords[1].value.attr)
+
+    return example, language
 
 def get_all_file_paths() -> list[str]:
     filepaths = []
@@ -71,26 +86,41 @@ def get_all_file_paths() -> list[str]:
 
 if __name__ == "__main__":
     bob = Bob()
-
     module_results = {}
 
-    module_filepaths = get_all_file_paths()
+    modules = [(name, module) for name, module in vars(F).items()
+        if (
+            isinstance(module, type)
+        )
+    ]
 
-    for module_filepath in module_filepaths:
+    for name, module in modules:
+        module_name = getattr(module, "__module__", None)
+        if module_name is None:
+            continue
+        mod = importlib.import_module(module_name)
+        module_file = getattr(mod, "__file__", None)
+        if module_file is None:
+            continue
         try:
-            example, language = extract_usage_example_ast(module_filepath)
-            if example == "None" or language == "None":
-                continue
+            example, language = extract_usage_example_ast(module_file)
+            if example is None or language is None:
+                if issubclass(module, Module):
+                    module_results[name] = "Fail: modules must have usage examples"
+                    print(f"Fail: {name} is a module without a usage example")
+                    continue
+                else:
+                    continue
             tree = parse_text_as_file(dedent(example))
             node = bob.build_ast(tree, TypeRef(["UsageExample"]))
             assert isinstance(node, L.Module)
-            print(f"Successfully built example for {module_filepath}")
-            module_results[module_filepath] = "PASS"
+            print(f"Successfully built example for {module_file}")
+            module_results[module_file] = "PASS"
         except* Exception as eg:
-            print(f"Error building example for {module_filepath}:")
+            print(f"Error building example for {module_file}:")
             for sub in eg.exceptions:
                 print(f"  - {repr(sub)}")
-            module_results[module_filepath] = "FAIL"
+            module_results[module_file] = "FAIL"
 
     # Count and print the number of passing examples out of total
     num_pass = sum(1 for result in module_results.values() if result == "PASS")
--- test/library/test_usage_examples.py
+++ test/library/test_usage_examples.py
@@ -1,6 +1,5 @@
 import ast
 import importlib
-import textwrap
 from pathlib import Path
 from textwrap import dedent
 
@@ -10,29 +9,41 @@ import faebryk.library._F as F
 from atopile.datatypes import TypeRef
 from atopile.front_end import Bob
 from atopile.parse import parse_text_as_file
-from faebryk.core.node import Node
+from faebryk.core.module import Module
+from faebryk.core.moduleinterface import ModuleInterface
 from faebryk.core.trait import Trait
 from faebryk.libs.library import L
 
 
-def _extract_usage_example_ast(file_path: str) -> tuple[str, str]:
+def _extract_usage_example_ast(file_path: str) -> tuple[str | None, str | None]:
     """
     Parse the usage example trait implementation and return (example, language).
     """
-    LIBRARY_PATH = Path(F.__file__).parent
-    file_path = LIBRARY_PATH / f"{file_path}"
+    example = None
+    language = None
 
-    content = file_path.read_text()
+    LIBRARY_PATH = Path(F.__file__).parent
+    abs_file_path = LIBRARY_PATH / f"{file_path}"
+
+    content = abs_file_path.read_text()
     tree = ast.parse(content)
     for node in ast.walk(tree):
-        if isinstance(node, ast.Assign):
-            for target in node.targets:
-                if isinstance(target, ast.Name):
-                    if target.id == "usage_example":
-                        example = node.value.keywords[0].value.value
-                        language = node.value.keywords[1].value.attr
-                        return example, language
-    return "None", "None"
+        if not isinstance(node, ast.Assign):
+            continue
+        if not any(isinstance(t, ast.Name) and t.id == "usage_example" for t in node.targets):
+            continue
+        if not isinstance(node.value, ast.Call):
+            continue
+        if not isinstance(node.value.keywords, list):
+            continue
+        if len(node.value.keywords) != 2:
+            continue
+        if isinstance(node.value.keywords[0].value, ast.Constant):
+            example = str(node.value.keywords[0].value.value)
+        if isinstance(node.value.keywords[1].value, ast.Attribute):
+            language = str(node.value.keywords[1].value.attr)
+
+    return example, language
 
 @pytest.mark.skipif(F is None, reason="Library not loaded")
 @pytest.mark.parametrize(
@@ -45,16 +56,31 @@ def _extract_usage_example_ast(file_path: str) -> tuple[str, str]:
         )
     ],
 )
-# @pytest.mark.timeout(60)  # TODO lower
 def test_usage_examples(name: str, module):
     """Test that all usage examples compile to graphs"""
     bob = Bob()
 
-    mod = importlib.import_module(module)
+    module_name = getattr(module, "__module__", None)
+    if module_name is None:
+        pytest.fail(f"Module {name} has no module name")
+    mod = importlib.import_module(module_name)
     module_file = getattr(mod, "__file__", None)
 
     if module_file is not None:
         example, language = _extract_usage_example_ast(module_file)
+
+        if example is None:
+            if issubclass(module, Module):
+                pytest.fail(f"{name} is a module without a usage example")
+            if issubclass(module, ModuleInterface):
+                pytest.skip(f"{name} is a module interface without a usage example")
+            if issubclass(module, Trait):
+                pytest.skip(f"{name} is a trait without a usage example")
+            pytest.skip(f"{name} has no usage example")
+
+        if language != F.has_usage_example.Language.ato.value:
+            pytest.skip(f"{name} has no usage example in ato language")
+
         tree = parse_text_as_file(dedent(example))
         node = bob.build_ast(tree, TypeRef(["UsageExample"]))
         assert isinstance(node, L.Module)
