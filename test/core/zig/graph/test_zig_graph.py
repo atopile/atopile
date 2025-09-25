f017ef7b9913e383afa6a3e9d89e4165da81b44c
iopapamanoglou <ioannis@atopile.io>
2025-09-24 21:26:08 -0700

    we graphin

--- test/core/zig/graph/test_zig_graph.py
+++ test/core/zig/graph/test_zig_graph.py
@@ -1,11 +1,48 @@
 def test_load_graph_module():
-    # Test import working
-    import faebryk.core.zig.gen.graph  # noqa: F401
-    from faebryk.core.zig.gen.graph.graph import GraphView
+    from faebryk.core.zig.gen.graph.graph import GraphView  # type: ignore  # noqa: F401
+
+
+def test_minimal_graph():
+    from faebryk.core.zig.gen.graph.graph import (  # type: ignore  # noqa: F401
+        Edge,
+        GraphView,
+        Node,
+    )
 
     g = GraphView.create()
-    print(repr(g))
+
+    n1 = Node.create()
+    n2 = Node.create()
+    n3 = Node.create()
+    e12 = Edge.create(n1, n2, 0)
+    e13 = Edge.create(n1, n3, 0)
+
+    bn1 = g.insert_node(n1)
+    g.insert_node(n2)
+    g.insert_node(n3)
+    g.insert_edge(e12)
+    g.insert_edge(e13)
+
+    # print(repr(g))
+
+    class Ctx:
+        def __init__(self):
+            self.edges = []
+
+    ctx = Ctx()
+
+    bn1.visit_edges_of_type(
+        edge_type=0,
+        T=Ctx,
+        ctx=ctx,
+        f=lambda ctx, edge: ctx.edges.append(edge.edge()),
+    )
+
+    assert len(ctx.edges) == 2
+
+    for edge in ctx.edges:
+        print(edge)
 
 
 if __name__ == "__main__":
-    test_load_graph_module()
+    test_minimal_graph()
