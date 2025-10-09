d90aa83bcffd3c9edc1ef4f49bf0173ab8e706a6
Sam Mellor <sam@atopile.io>
2025-10-09 11:48:55 -0700

    stub module and mif methods

--- src/faebryk/core/module.py
+++ src/faebryk/core/module.py
@@ -3,11 +3,10 @@
 import logging
 from typing import TYPE_CHECKING, Callable, Iterable
 
-from faebryk.core.cpp import GraphInterfaceModuleSibling
-from faebryk.core.node import Node, NodeException, f_field
+from deprecated import deprecated
+
+from faebryk.core.node import Node, NodeException
 from faebryk.core.trait import Trait
-from faebryk.libs.exceptions import accumulate
-from faebryk.libs.util import cast_assert, unique_ref
 
 if TYPE_CHECKING:
     from faebryk.core.moduleinterface import ModuleInterface
@@ -25,28 +24,6 @@ class ModuleException(NodeException):
 class Module(Node):
     class TraitT(Trait): ...
 
-    specializes = f_field(GraphInterfaceModuleSibling)(is_parent=False)
-    specialized = f_field(GraphInterfaceModuleSibling)(is_parent=True)
-
-    def get_most_special(self) -> "Module":
-        specialers = {
-            specialer
-            for specialer_gif in self.specialized.get_gif_edges()
-            if (specialer := specialer_gif.node) is not self
-            and isinstance(specialer, Module)
-        }
-        if not specialers:
-            return self
-
-        specialest_next = unique_ref(
-            specialer.get_most_special() for specialer in specialers
-        )
-
-        assert len(specialest_next) == 1, (
-            f"Ambiguous specialest {specialest_next} for {self}"
-        )
-        return next(iter(specialest_next))
-
     def get_children_modules[T: Module](
         self: Node,
         types: type[T] | tuple[type[T], ...],
@@ -56,60 +33,15 @@ class Module(Node):
         f_filter: Callable[[T], bool] | None = None,
         sort: bool = True,
     ) -> set[T]:
-        out = self.get_children(
-            direct_only=direct_only,
-            types=types,
-            include_root=include_root,
-            f_filter=f_filter,
-            sort=sort,
-        )
-        out_specialized = {
-            n.get_most_special()
-            for n in self.get_children(
+        return set(
+            self.get_children(
                 direct_only=direct_only,
-                types=Module,
+                types=types,
                 include_root=include_root,
-                f_filter=lambda x: x.get_most_special() != x,
+                f_filter=f_filter,
                 sort=sort,
             )
-        }
-        if most_special:
-            special_out = set()
-            todo = out_specialized
-            # TODO can be done more efficiently by just allowing in the graph search
-            # specialize edges
-            for n in out:
-                # Filter out children of specialized modules
-                has_specialized_parent = n.get_parent_f(
-                    lambda x: isinstance(x, Module) and x.get_most_special() != x
-                )
-                if has_specialized_parent:
-                    continue
-                n_special = n.get_most_special()
-                # Non-special can just pass
-                if n_special is n:
-                    special_out.add(n_special)
-                    continue
-                # Already processed
-                if n_special in out | special_out:
-                    continue
-                # To process children
-                todo.add(n)
-
-            for n_special in todo:
-                special_out.update(
-                    n_special.get_children_modules(
-                        types=types,
-                        most_special=True,
-                        direct_only=direct_only,
-                        include_root=True,
-                        f_filter=f_filter,
-                        sort=sort,
-                    )
-                )
-            out = special_out
-
-        return out
+        )
 
     class InvalidSpecializationError(Exception):
         """Cannot specialize module with special"""
@@ -128,104 +60,9 @@ class Module(Node):
         matrix: list[tuple["ModuleInterface", "ModuleInterface"]] | None = None,
         attach_to: Node | None = None,
     ) -> T:
-        from faebryk.core.moduleinterface import ModuleInterface
-        from faebryk.core.parameter import Parameter
+        raise NotImplementedError("zig core - specialization is not implemented")
 
-        logger.debug(f"Specializing Module {self} with {special}" + " " + "=" * 20)
-
-        def get_node_prop_matrix[N: Node](sub_type: type[N]):
-            return list(self.zip_children_by_name_with(special, sub_type).values())
-
-        if matrix is None:
-            matrix = get_node_prop_matrix(ModuleInterface)
-
-        # TODO add warning if not all src interfaces used
-        param_matrix = get_node_prop_matrix(Parameter)
-
-        err_acc = accumulate(self.InvalidSpecializationError)
-
-        for src, dst in matrix:
-            with err_acc.collect():
-                if src is None:
-                    continue
-                if dst is None:
-                    raise self.InvalidSpecializationError(
-                        f"Special module misses interface: {src.get_name()}",
-                        module=self,
-                        special=special,
-                    )
-                src.specialize(dst)
-
-        for src, dst in param_matrix:
-            with err_acc.collect():
-                if src is None:
-                    continue
-                if dst is None:
-                    raise self.InvalidSpecializationError(
-                        f"Special module misses parameter: {src.get_name()}",
-                        module=self,
-                        special=special,
-                    )
-                dst.alias_is(src)
-
-        err_acc.raise_errors()
-
-        # TODO this cant work
-        # for t in self.traits:
-        #    # TODO needed?
-        #    if special.has_trait(t.trait):
-        #        continue
-        #    special.add(t)
-
-        self.specialized.connect(special.specializes)
-
-        # Attach to new parent
-        has_parent = special.get_parent() is not None
-        assert not has_parent or attach_to is None
-        if not has_parent:
-            if attach_to:
-                attach_to.add(special, container=attach_to.specialized_)
-            else:
-                gen_parent = self.get_parent()
-                if gen_parent:
-                    cast_assert(Node, gen_parent[0]).add(
-                        special, name=f"{gen_parent[1]}_specialized"
-                    )
-
-        return special
-
-    @staticmethod
-    def connect_all_interfaces_by_name(
-        src: Iterable["Module"] | "Module",
-        dst: Iterable["Module"] | "Module",
-        allow_partial: bool = False,
-    ):
-        from faebryk.core.moduleinterface import ModuleInterface
-
-        if isinstance(src, Module):
-            src = [src]
-        if isinstance(dst, Module):
-            dst = [dst]
-
-        for src_, dst_ in zip(src, dst):
-            for k, (src_m, dst_m) in src_.zip_children_by_name_with(
-                dst_, ModuleInterface
-            ).items():
-                # TODO: careful this also connects runtime children
-                # for now skip stuff prefixed with _
-                if k.startswith("_"):
-                    continue
-                if src_m is None or dst_m is None:
-                    if not allow_partial:
-                        raise Exception(f"Node with name {k} not present in both")
-                    continue
-                src_m.connect(dst_m)
-
-    def connect_interfaces_by_name(self, *dst: "Module", allow_partial: bool = False):
-        type(self).connect_all_interfaces_by_name(
-            self, dst, allow_partial=allow_partial
-        )
-
+    @deprecated("TODO: static helper function")
     def get_parameters(self) -> list["Parameter"]:
         from faebryk.core.parameter import Parameter
 
--- src/faebryk/core/moduleinterface.py
+++ src/faebryk/core/moduleinterface.py
@@ -1,31 +1,16 @@
 # This file is part of the faebryk project
 # SPDX-License-Identifier: MIT
-import logging
-from itertools import pairwise
-from typing import (
-    Iterable,
-    Self,
-    Sequence,
-    cast,
-)
+from __future__ import annotations
 
-from faebryk.core.cpp import (
-    GraphInterfaceModuleConnection,
-    Path,
-)
-from faebryk.core.graphinterface import GraphInterface
-from faebryk.core.link import (
-    Link,
-    LinkDirect,
-    LinkDirectConditional,
-    LinkDirectConditionalFilterResult,
-    LinkDirectDerived,
-)
-from faebryk.core.node import CNode, Node, NodeException
-from faebryk.core.pathfinder import find_paths
+import logging
+from typing import TYPE_CHECKING, Iterable, Self, Sequence
+
+from faebryk.core.node import Node
 from faebryk.core.trait import Trait
-from faebryk.library.can_specialize import can_specialize
-from faebryk.libs.util import ConfigFlag, cast_assert, groupby, once
+from faebryk.libs.util import ConfigFlag
+
+if TYPE_CHECKING:
+    from faebryk.core.link import Link
 
 logger = logging.getLogger(__name__)
 
@@ -35,107 +20,72 @@ IMPLIED_PATHS = ConfigFlag("IMPLIED_PATHS", default=False, descr="Use implied pa
 type Bridgable[T: "ModuleInterface"] = Node | T
 
 
+class _ModuleInterfaceEdgeStub:
+    def __init__(self, owner: "ModuleInterface", kind: str) -> None:
+        self._owner = owner
+        self._kind = kind
+
+    def _fail(self, operation: str) -> None:
+        raise NotImplementedError(
+            f"TODO: Zig core migration – ModuleInterface.{self._kind}.{operation} "
+            "is not implemented"
+        )
+
+    def connect(self, *args, **kwargs):
+        self._fail("connect")
+
+    def is_connected_to(self, *args, **kwargs):
+        self._fail("is_connected_to")
+
+    def get_connected_nodes(self, *args, **kwargs):
+        self._fail("get_connected_nodes")
+
+    def __getattr__(self, name: str):
+        self._fail(name)
+
+    def __repr__(self) -> str:
+        return (
+            f"<ModuleInterfaceEdgeStub kind={self._kind} "
+            f"owner={type(self._owner).__name__}>"
+        )
+
+
 class ModuleInterface(Node):
     class TraitT(Trait): ...
 
-    specializes: GraphInterface
-    specialized: GraphInterface
-    connected: GraphInterfaceModuleConnection
-
-    # TODO: move to cpp
-    class _LinkDirectShallow(LinkDirectConditional):
-        """
-        Make link that only connects up but not down
-        """
-
-        def is_childtype_of_test_type(self, node: CNode):
-            return isinstance(node, self.children_types)
-            # return type(node) in self.children_types
-
-        def check_path(self, path: Path) -> LinkDirectConditionalFilterResult:
-            out = (
-                LinkDirectConditionalFilterResult.FILTER_PASS
-                if not self.is_childtype_of_test_type(path[0].node)
-                else LinkDirectConditionalFilterResult.FILTER_FAIL_UNRECOVERABLE
-            )
-            return out
-
-        def __init__(self, test_type: type["ModuleInterface"]):
-            self.test_type = test_type
-            # TODO this is a bit of a hack to get the children types
-            #  better to do on set_connections
-            self.children_types = tuple(
-                type(c)
-                for c in test_type().get_children(
-                    direct_only=False, types=ModuleInterface, include_root=False
-                )
-            )
-            super().__init__(
-                self.check_path,
-                needs_only_first_in_path=True,
-            )
-
-    @classmethod
-    @once
-    def LinkDirectShallow(cls):
-        class _LinkDirectShallowMif(ModuleInterface._LinkDirectShallow):
-            def __init__(self):
-                super().__init__(test_type=cls)
-
-        return _LinkDirectShallowMif
-
     def __preinit__(self) -> None: ...
 
+    @classmethod
+    def LinkDirectShallow(cls):
+        raise NotImplementedError(
+            "TODO: Zig core migration – ModuleInterface.LinkDirectShallow is not "
+            "available"
+        )
+
+    @property
+    def specializes(self) -> _ModuleInterfaceEdgeStub:
+        return self._get_edge_stub("specializes")
+
+    @property
+    def specialized(self) -> _ModuleInterfaceEdgeStub:
+        return self._get_edge_stub("specialized")
+
+    @property
+    def connected(self) -> _ModuleInterfaceEdgeStub:
+        return self._get_edge_stub("connected")
+
+    def _get_edge_stub(self, name: str) -> _ModuleInterfaceEdgeStub:
+        attr = f"_{name}_stub"
+        if not hasattr(self, attr):
+            setattr(self, attr, _ModuleInterfaceEdgeStub(owner=self, kind=name))
+        return getattr(self, attr)
+
     def connect(
-        self: Self, *other: Self, link: type[Link] | Link | None = None
+        self: Self, *other: Self, link: type["Link"] | "Link" | None = None
     ) -> Self:
-        mismatched = [o for o in other if type(o) is not type(self)]
-        if mismatched:
-            if len(mismatched) == 1:
-                raise NodeException(
-                    self,
-                    (
-                        f"Cannot connect module `{self}` "
-                        f"(type `{type(self).__name__}`) "
-                        f"with module `{mismatched[0]}` "
-                        f"(type `{type(mismatched[0]).__name__}`). "
-                        "Modules must be of the same type."
-                    ),
-                )
-            else:
-                mismatches_str = ", ".join(
-                    f"{m} (type {type(m).__name__})" for m in mismatched
-                )
-                raise NodeException(
-                    self,
-                    (
-                        f"Cannot connect module {self} (type {type(self).__name__}) "
-                        f"with modules: {mismatches_str}. "
-                        "Modules must be of the same type."
-                    ),
-                )
-
-        # TODO: consider returning self always
-        # - con: if construcing anonymous stuff in connection no ref
-        # - pro: more intuitive
-        ret = other[-1] if other else self
-
-        if link is None:
-            link = LinkDirect
-        if isinstance(link, type):
-            link = link()
-
-        # resolve duplicate links
-        new_links = [
-            o.connected
-            for o in other
-            if not (existing_link := self.connected.is_connected_to(o.connected))
-            or existing_link != link
-        ]
-
-        self.connected.connect(new_links, link=link)
-
-        return ret
+        raise NotImplementedError(
+            "TODO: Zig core migration – ModuleInterface.connect is not implemented"
+        )
 
     def connect_via(
         self,
@@ -143,151 +93,47 @@ class ModuleInterface(Node):
         *other: Self,
         link=None,
     ):
-        from faebryk.library.can_bridge import can_bridge
-
-        bridges = [bridge] if isinstance(bridge, Node | ModuleInterface) else bridge
-        intf = self
-        for sub_bridge in bridges:
-            if isinstance(sub_bridge, type(self)):
-                intf.connect(sub_bridge, link=link)
-            else:
-                t = sub_bridge.get_trait(can_bridge)
-                intf.connect(t.get_in(), link=link)
-                intf = t.get_out()
-
-        intf.connect(*other, link=link)
+        raise NotImplementedError(
+            "TODO: Zig core migration – ModuleInterface.connect_via is not implemented"
+        )
 
     def connect_shallow(self, *other: Self) -> Self:
-        # TODO: clone limitation, waiting for c++ LinkShallow
-        if len(other) > 1:
-            for o in other:
-                self.connect_shallow(o)
-            return self
+        raise NotImplementedError(
+            "TODO: Zig core migration – ModuleInterface.connect_shallow is not implemented"
+        )
 
-        return self.connect(*other, link=type(self).LinkDirectShallow())
+    def get_connected(self, include_self: bool = False) -> dict[Self, object]:
+        raise NotImplementedError(
+            "TODO: Zig core migration – ModuleInterface.get_connected is not implemented"
+        )
 
-    def get_connected(self, include_self: bool = False) -> dict[Self, Path]:
-        paths = find_paths(self, [])
-        # TODO theoretically we could get multiple paths for the same MIF
-        # practically this won't happen in the current implementation
-        paths_per_mif = groupby(paths, lambda p: cast_assert(type(self), p[-1].node))
-
-        def choose_path(_paths: list[Path]) -> Path:
-            return self._path_with_least_conditionals(_paths)
-
-        path_per_mif = {
-            mif: choose_path(paths)
-            for mif, paths in paths_per_mif.items()
-            if mif is not self or include_self
-        }
-        if include_self:
-            assert self in path_per_mif
-        for mif, paths in paths_per_mif.items():
-            self._connect_via_implied_paths(mif, paths)
-        return path_per_mif
-
-    def is_connected_to(self, other: "ModuleInterface") -> list[Path]:
-        return [
-            path for path in find_paths(self, [other]) if path[-1] is other.self_gif
-        ]
+    def is_connected_to(self, other: "ModuleInterface") -> list[object]:
+        raise NotImplementedError(
+            "TODO: Zig core migration – ModuleInterface.is_connected_to is not implemented"
+        )
 
     def specialize[T: ModuleInterface](self, special: T) -> T:
-        logger.debug(f"Specializing MIF {self} with {special}")
-
-        extra = set()
-        # allow non-base specialization if explicitly allowed
-        if special.has_trait(can_specialize):
-            extra = set(special.get_trait(can_specialize).get_specializable_types())
-
-        if not (
-            isinstance(special, type(self))
-            or any(issubclass(t, type(self)) for t in extra)
-            # TODO: remove, this is a hack for ato
-            or type(special).__name__ == type(self).__name__
-        ):
-            type_self_id = f"{type(self).__name__}|{id(type(self)):02X}"
-            type_special_id = f"{type(special).__name__}|{id(type(special)):02X}"
-            raise TypeError(
-                f"Cannot specialize {self}(type={type_self_id}) with "
-                f"{special}(type={type_special_id}). Specializable types: {extra}"
-            )
-
-        # This is doing the heavy lifting
-        self.connected.connect(special.connected)
-
-        # Establish sibling relationship
-        self.specialized.connect(special.specializes)
-
-        return cast(T, special)
-
-    # def get_general(self):
-    #    out = self.specializes.get_parent()
-    #    if out:
-    #        return out[0]
-    #    return None
-
-    # FIXME
-    # def __init_subclass__(cls, *, init: bool = True) -> None:
-    #     if hasattr(cls, "_on_connect"):
-    #         raise TypeError("Overriding _on_connect is deprecated")
-
-    #     return super().__init_subclass__(init=init)
+        raise NotImplementedError(
+            "TODO: Zig core migration – ModuleInterface.specialize is not implemented"
+        )
 
     @staticmethod
-    def _path_with_least_conditionals(paths: list["Path"]) -> "Path":
-        if len(paths) == 1:
-            return paths[0]
+    def _path_with_least_conditionals(paths: list[object]) -> object:
+        raise NotImplementedError(
+            "TODO: Zig core migration – ModuleInterface path resolution is not implemented"
+        )
 
-        paths_links = [
-            (
-                path,
-                [
-                    e1.is_connected_to(e2)
-                    for e1, e2 in pairwise(cast(Iterable[GraphInterface], path))
-                ],
-            )
-            for path in paths
-        ]
-        paths_conditionals = [
-            (
-                path,
-                [link for link in links if isinstance(link, LinkDirectConditional)],
-            )
-            for path, links in paths_links
-        ]
-        path = min(paths_conditionals, key=lambda x: len(x[1]))[0]
-        return path
-
-    def _connect_via_implied_paths(self, other: Self, paths: list["Path"]):
-        if not IMPLIED_PATHS:
-            return
-
-        if self is other:
-            return
-
-        if self.connected.is_connected_to(other.connected):
-            # TODO link resolution
-            return
-
-        # heuristic: choose path with fewest conditionals
-        path = self._path_with_least_conditionals(paths)
-
-        self.connect(other, link=LinkDirectDerived(path))
+    def _connect_via_implied_paths(self, other: Self, paths: list[object]):
+        raise NotImplementedError(
+            "TODO: Zig core migration – ModuleInterface implied path hookup is not "
+            "implemented"
+        )
 
     @staticmethod
     def _group_into_buses[T: ModuleInterface](mifs: Iterable[T]) -> dict[T, set[T]]:
-        """
-        returns dict[BusRepresentative, set[MIFs in Bus]]
-        """
-        to_check = set(mifs)
-        buses = {}
-        while to_check:
-            interface = to_check.pop()
-            ifs = interface.get_connected(include_self=True)
-            buses[interface] = ifs
-            to_check.difference_update(ifs.keys())
-
-        return buses
+        raise NotImplementedError(
+            "TODO: Zig core migration – ModuleInterface bus grouping is not implemented"
+        )
 
     # TODO get rid of this abomination
     @property
