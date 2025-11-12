04d578459bd70152a2956880257ff2ed2a7a00b4
Ray Allen <ray@atopile.io>
2025-11-12 15:30:04 -0800

    cleanup i2c

--- src/faebryk/library/I2C.py
+++ src/faebryk/library/I2C.py
@@ -1,6 +1,7 @@
 # This file is part of the faebryk project
 # SPDX-License-Identifier: MIT
 import logging
+from enum import Enum
 
 import faebryk.core.node as fabll
 import faebryk.library._F as F
@@ -9,6 +10,15 @@ logger = logging.getLogger(__name__)
 
 
 class I2C(fabll.Node):
+    # ----------------------------------------
+    #                 enums
+    # ----------------------------------------
+    class SpeedMode(Enum):
+        low_speed = 10  # * P.khertz
+        standard_speed = 100  # * P.khertz
+        fast_speed = 400  # * P.khertz
+        high_speed = 3.4  # * P.Mhertz
+
     # ----------------------------------------
     #     modules, interfaces, parameters
     # ----------------------------------------
@@ -39,23 +49,11 @@ class I2C(fabll.Node):
             ),
         )
 
-    # def bus_crosses_pad_boundary(self):
-    #     return (
-    #         self.scl.line.net_crosses_pad_boundary()
-    #         or self.sda.line.net_crosses_pad_boundary()
-    #     )
-
     # def terminate(self, owner: fabll.Node):
     #     # TODO: https://www.ti.com/lit/an/slva689/slva689.pdf
 
     #     self.pull_up_sda = self.sda.pulled.pull(up=True, owner=owner)
     #     self.pull_up_scl = self.scl.pulled.pull(up=True, owner=owner)
-
-    # class SpeedMode(Enum):
-    #     low_speed = 10  # * P.khertz
-    #     standard_speed = 100  # * P.khertz
-    #     fast_speed = 400  # * P.khertz
-    #     high_speed = 3.4  # * P.Mhertz
 
     # @staticmethod
     # def define_max_frequency_capability(mode: SpeedMode):
