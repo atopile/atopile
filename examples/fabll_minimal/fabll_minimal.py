b5c137551a4135de09f4be0f7f5e613176088c3b
ruben-iteng <94007802+ruben-iteng@users.noreply.github.com>
2025-12-12 14:58:27 -0800

    extend fabll_minimal example

--- examples/fabll_minimal/fabll_minimal.py
+++ examples/fabll_minimal/fabll_minimal.py
@@ -1,15 +1,21 @@
 import faebryk.core.faebrykpy as fbrk
-import faebryk.core.graph as graph
 import faebryk.core.node as fabll
 import faebryk.library._F as F
 
 
 class App(fabll.Node):
-    r1 = F.Resistor.MakeChild()
-    r2 = F.Resistor.MakeChild()
+    res1 = F.Resistor.MakeChild()
+    res2 = F.Resistor.MakeChild()
 
-    x = fabll.MakeEdge(
-        lhs=[r1, F.Resistor.unnamed[0]],
-        rhs=[r2, F.Resistor.unnamed[1]],
-        edge=fbrk.EdgeInterfaceConnection.build(shallow=False)
+    r2r = fabll.MakeEdge(
+        [res1, F.Resistor.unnamed[0]],
+        [res2, F.Resistor.unnamed[1]],
+        edge=fbrk.EdgeInterfaceConnection.build(shallow=False),
+    )
+
+    res1_constraint = F.Literals.Numbers.MakeChild_ConstrainToSubsetLiteral(
+        [res1, F.Resistor.resistance], 200, 400, unit=F.Units.Ohm
+    )
+    res2_constraint = F.Literals.Numbers.MakeChild_ConstrainToSubsetLiteral(
+        [res2, F.Resistor.resistance], 15, 40, unit=F.Units.Ohm
     )
