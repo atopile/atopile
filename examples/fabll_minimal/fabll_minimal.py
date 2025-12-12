884c5ae9ab18dd0e28943bf18ec69e926837a0f6
Ray Allen <ray@atopile.io>
2025-12-12 11:32:24 -0800

    resistors connected in fabll

--- examples/fabll_minimal/fabll_minimal.py
+++ examples/fabll_minimal/fabll_minimal.py
@@ -1,20 +1,15 @@
 import faebryk.core.faebrykpy as fbrk
+import faebryk.core.graph as graph
 import faebryk.core.node as fabll
 import faebryk.library._F as F
 
 
 class App(fabll.Node):
     r1 = F.Resistor.MakeChild()
-    # r2 = F.Resistor.MakeChild()
+    r2 = F.Resistor.MakeChild()
 
-    # @classmethod
-    # def MakeChild(cls):
-    #     out = fabll._ChildField(cls)
-    #     out.add_dependant(
-    #         fabll.MakeEdge(
-    #             lhs=[cls.r1.get().unnamed[0]],
-    #             rhs=[cls.r2.get().unnamed[0]],
-    #             edge=fbrk.EdgeInterfaceConnection.build()
-    #         )
-    #     )
-    #     return out
+    x = fabll.MakeEdge(
+        lhs=[r1, F.Resistor.unnamed[0]],
+        rhs=[r2, F.Resistor.unnamed[1]],
+        edge=fbrk.EdgeInterfaceConnection.build(shallow=False)
+    )
