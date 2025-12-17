1fbe44e5dd0e7c0942ca7a08e4edc3fd979efed6
Narayan Powderly <narayan@atopile.io>
2025-12-17 11:03:10 -0800

    remove _ from size_ param in has_package_requirements

--- src/faebryk/library/has_package_requirements.py
+++ src/faebryk/library/has_package_requirements.py
@@ -16,11 +16,11 @@ class has_package_requirements(fabll.Node):
 
     is_trait = fabll.Traits.MakeEdge(fabll.ImplementsTrait.MakeChild().put_on_type())
     is_immutable = fabll.Traits.MakeEdge(fabll.is_immutable.MakeChild()).put_on_type()
-    size_ = F.Parameters.EnumParameter.MakeChild(enum_t=SMDSize)
+    size = F.Parameters.EnumParameter.MakeChild(enum_t=SMDSize)
 
     def get_sizes(self, solver: Solver) -> list[SMDSize]:
         # TODO: use solver to get size
-        sizes = self.size_.get().force_extract_literal().get_values_typed(SMDSize)
+        sizes = self.size.get().force_extract_literal().get_values_typed(SMDSize)
 
         return sizes
 
@@ -29,7 +29,7 @@ class has_package_requirements(fabll.Node):
         out = fabll._ChildField(cls)
         out.add_dependant(
             F.Literals.AbstractEnums.MakeChild_ConstrainToLiteral(
-                [out, cls.size_],
+                [out, cls.size],
                 size,
             )
         )
