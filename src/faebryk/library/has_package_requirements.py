6213c3ab46e1fda0fc5d924e255d590eef094c85
Sam Mellor <sam@atopile.io>
2025-12-18 18:58:08 -0800

    get package req sizes w/o solver

--- src/faebryk/library/has_package_requirements.py
+++ src/faebryk/library/has_package_requirements.py
@@ -3,10 +3,7 @@
 
 import faebryk.core.node as fabll
 import faebryk.library._F as F
-from faebryk.core.solver.solver import Solver
 from faebryk.libs.smd import SMDSize
-
-# from faebryk.libs.util import cast_assert
 
 
 class has_package_requirements(fabll.Node):
@@ -18,11 +15,8 @@ class has_package_requirements(fabll.Node):
     is_immutable = fabll.Traits.MakeEdge(fabll.is_immutable.MakeChild()).put_on_type()
     size = F.Parameters.EnumParameter.MakeChild(enum_t=SMDSize)
 
-    def get_sizes(self, solver: Solver) -> list[SMDSize]:
-        # TODO: use solver to get size
-        sizes = self.size.get().force_extract_literal().get_values_typed(SMDSize)
-
-        return sizes
+    def get_sizes(self) -> list[SMDSize]:
+        return self.size.get().force_extract_literal().get_values_typed(SMDSize)
 
     @classmethod
     def MakeChild(cls, size: SMDSize):  # type: ignore[invalid-method-override]
