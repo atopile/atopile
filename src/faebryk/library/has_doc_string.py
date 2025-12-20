48158aa88a3ed5cbd47d661bc913de6861a87508
Narayan Powderly <narayan@atopile.io>
2025-12-19 17:55:53 -0800

    add has_doc_string trait

---
+++ src/faebryk/library/has_doc_string.py
@@ -0,0 +1,45 @@
+# This file is part of the faebryk project
+# SPDX-License-Identifier: MIT
+
+from typing import Any
+
+import faebryk.core.node as fabll
+import faebryk.library._F as F
+
+
+class has_doc_string(fabll.Node):
+    """
+    Trait for modules/components/interfaces that have a docstring.
+
+    This trait is automatically attached when a block has a docstring
+    (triple-quoted string statement as the first statement in the block).
+
+    Example ato usage:
+        module MyModule:
+            '''
+            This is the docstring for MyModule.
+            It can span multiple lines.
+            '''
+            r = new Resistor
+    """
+
+    is_trait = fabll.Traits.MakeEdge(fabll.ImplementsTrait.MakeChild().put_on_type())
+    is_immutable = fabll.Traits.MakeEdge(fabll.is_immutable.MakeChild()).put_on_type()
+
+    doc_string_ = F.Parameters.StringParameter.MakeChild()
+
+    @property
+    def doc_string(self) -> str:
+        """Get the docstring text."""
+        return str(self.doc_string_.get().force_extract_literal().get_values()[0])
+
+    @classmethod
+    def MakeChild(cls, doc_string: str) -> fabll._ChildField[Any]:
+        """Create a has_doc_string trait with the given docstring text."""
+        out = fabll._ChildField(cls)
+        out.add_dependant(
+            F.Literals.Strings.MakeChild_ConstrainToLiteral(
+                [out, cls.doc_string_], doc_string
+            )
+        )
+        return out
