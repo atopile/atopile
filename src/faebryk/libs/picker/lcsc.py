cdac1f46ca5c2481f0cfeb0337f702cdfd5b1e2c
RubenB-ITENG <94007802+ruben-iteng@users.noreply.github.com>
2026-02-20 18:47:51 +0100

    LCSC: Fix 0201 smd 3D model x alignment (#1701)
    <!--
    Ensure PR title follows the correct format:
    - Scope prefix first (capitalized)
    - Colon separator
    - Action verb (Fix, Add, Update, Move, etc.)
    - Clear description of what changed
    
    e.g.
    VSCE: Add 3D model viewer
    Library: Remove layout trait from crystal oscillator
    Buildutil: Split out GLB export into new `3d-model` target
    -->
    
    ## Description
    
    0201 parts don't have the `smd` fp_type so they were not fixed 
    
    ## Motivation
    
    fixes https://github.com/atopile/atopile/issues/1498

--- src/faebryk/libs/picker/lcsc.py
+++ src/faebryk/libs/picker/lcsc.py
@@ -457,7 +457,9 @@ def _fix_3d_model_offsets(ki_footprint: ExporterFootprintKicad):
         return
 
     if WORKAROUND_SMD_3D_MODEL_FIX:
-        if ki_footprint.input.info.fp_type == "smd":
+        if ki_footprint.input.info.fp_type == "smd" or re.search(
+            r"[cCrR]0201", ki_footprint.input.info.name
+        ):
             ki_footprint.output.model_3d.translation.x = 0
             ki_footprint.output.model_3d.translation.y = 0
     if WORKAROUND_THT_INCH_MM_SWAP_FIX:
