470e26680a5791279d1c2022dc8f1dc551eebf03
ruben-iteng <94007802+ruben-iteng@users.noreply.github.com>
2026-02-23 13:09:35 +0100

    LIBRARY: Add bus spec and role traits

--- src/faebryk/library/_F.py
+++ src/faebryk/library/_F.py
@@ -56,6 +56,7 @@ from faebryk.library.is_alias_bus_parameter import is_alias_bus_parameter
 from faebryk.library.is_sum_bus_parameter import is_sum_bus_parameter
 from faebryk.library.Filter import Filter
 import faebryk.library.Lead as Lead
+import faebryk.library.bus as bus
 from faebryk.library.has_net_name_affix import has_net_name_affix
 from faebryk.library.requires_external_usage import requires_external_usage
 from faebryk.library.XtalIF import XtalIF
@@ -158,6 +159,7 @@ __all__ = [
     "is_sum_bus_parameter",
     "Filter",
     "Lead",
+    "bus",
     "has_net_name_affix",
     "requires_external_usage",
     "XtalIF",
