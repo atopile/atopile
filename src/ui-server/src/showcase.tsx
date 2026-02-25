44e22bfecdf64571470f13bd22d321d66ae76300
ruben-iteng <94007802+ruben-iteng@users.noreply.github.com>
2026-02-25 12:34:22 +0100

    UI: Add light-dark mode toggle to component showcase

--- src/ui-server/src/showcase.tsx
+++ src/ui-server/src/showcase.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ComponentShowcase } from './components/ComponentShowcase'
+import { initializeTheme } from './hooks/useTheme'
 import './styles/index.css'
+
+initializeTheme();
 
 const root = document.getElementById('root')
 if (root) {
